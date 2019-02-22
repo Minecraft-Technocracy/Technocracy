9a33f9a955c0f2c2313c5bfcbda02949993fcbae
Cydhra <ubezl@student.kit.edu>
2019-02-22 18:40:45 +0100

    Initial Commit

---
+++ foundation/src/main/kotlin/net/cydhra/technocracy/TCFoundation.kt
@@ -0,0 +1,39 @@
+package net.cydhra.technocracy
+
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import org.apache.logging.log4j.Logger
+
+@Mod(modid = TCFoundation.MODID, name = TCFoundation.NAME, version = TCFoundation.VERSION,
+        modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
+object TCFoundation {
+
+    private lateinit var logger: Logger
+
+    /**
+     * Module-internal constant for the forge mod identifier
+     */
+    internal const val MODID = "##MOD.ID"
+
+    /**
+     * Mod version
+     */
+    const val VERSION = "##MOD.VERSION"
+
+    /**
+     * Mod name used by forge
+     */
+    const val NAME = "Technocracy Foundation"
+
+    @EventHandler
+    fun preInit(event: FMLPreInitializationEvent) {
+        logger = event.modLog
+    }
+
+    @EventHandler
+    fun init(event: FMLInitializationEvent) {
+
+    }
+}
