f9d57a93128cff6932fadaaf03eca78db9d6ed2f
Cydhra <ubezl@student.kit.edu>
2019-02-22 21:41:06 +0100

    implemented model registration

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/items/ItemManager.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/items/ItemManager.kt
@@ -2,7 +2,10 @@
 package net.cydhra.technocracy.foundation.items
 
 import net.cydhra.technocracy.foundation.TCFoundation
+import net.minecraft.client.renderer.block.model.ModelResourceLocation
 import net.minecraft.item.Item
+import net.minecraftforge.client.event.ModelRegistryEvent
+import net.minecraftforge.client.model.ModelLoader
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -35,6 +38,28 @@ object ItemManager {
     fun registerItems(event: RegistryEvent.Register<Item>) {
         event.registry.registerAll(*this.itemsToRegister.toTypedArray())
 
-        this.itemsToRegister.clear()
+    }
+
+    fun registerRenders(event: ModelRegistryEvent) {
+        this.itemsToRegister.forEach(this::registerItemRender)
+    }
+
+    /**
+     * Register a custom model resource location for an item.
+     *
+     * @param item a [BaseItem] instance
+     */
+    private fun registerItemRender(item: BaseItem) {
+        this.registerItemRender(item, 0)
+    }
+
+    /**
+     * Register a custom model resource location for an item.
+     *
+     * @param item a [BaseItem] instance
+     * @param metadata the item metadata which uses this model resource
+     */
+    private fun registerItemRender(item: BaseItem, metadata: Int) {
+        ModelLoader.setCustomModelResourceLocation(item, metadata, ModelResourceLocation(item.registryName!!, "inventory"))
     }
 }
