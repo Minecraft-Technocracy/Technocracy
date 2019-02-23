8e784c3edb2db2e9ee3152c9d03df57c8952e40e
Cydhra <ubezl@student.kit.edu>
2019-02-23 23:47:03 +0100

    implemented IItemColor for constant item color tints

---
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/items/ConstantItemColor.kt
@@ -0,0 +1,13 @@
+package net.cydhra.technocracy.foundation.items
+
+import net.minecraft.client.renderer.color.IItemColor
+import net.minecraft.item.ItemStack
+
+/**
+ * An entity that returns a constant value as item color for immutably tinted items
+ */
+class ConstantItemColor(private val constantItemColor: Int) : IItemColor {
+    override fun colorMultiplier(stack: ItemStack, tintIndex: Int): Int {
+        return this.constantItemColor
+    }
+}
