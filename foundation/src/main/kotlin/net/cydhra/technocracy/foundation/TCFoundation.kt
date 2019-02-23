a0f06b760683f2334d9ceab556461d443d19aeab
Cydhra <ubezl@student.kit.edu>
2019-02-23 23:56:48 +0100

    proxy initialization from mod instance

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/TCFoundation.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/TCFoundation.kt
@@ -1,16 +1,19 @@
 package net.cydhra.technocracy.foundation
 
+import net.cydhra.technocracy.foundation.proxy.ClientProxy
+import net.cydhra.technocracy.foundation.proxy.CommonProxy
+import net.cydhra.technocracy.foundation.proxy.ISidedProxy
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import net.minecraftforge.fml.relauncher.Side
 import org.apache.logging.log4j.Logger
 
 @Mod(modid = TCFoundation.MODID, name = TCFoundation.NAME, version = TCFoundation.VERSION,
         modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
 object TCFoundation {
-
-    private lateinit var logger: Logger
 
     /**
      * Module-internal constant for the forge mod identifier
@@ -27,13 +30,28 @@ object TCFoundation {
      */
     const val NAME = "Technocracy Foundation"
 
+    /**
+     * Mod logger
+     */
+    private lateinit var logger: Logger
+
+    private lateinit var proxy: ISidedProxy
+
     @EventHandler
     fun preInit(event: FMLPreInitializationEvent) {
         logger = event.modLog
+        proxy = if (event.side == Side.CLIENT) ClientProxy() else CommonProxy()
+
+        proxy.preInit()
     }
 
     @EventHandler
     fun init(event: FMLInitializationEvent) {
+        proxy.init()
+    }
 
+    @EventHandler
+    fun postInit(event: FMLPostInitializationEvent) {
+        proxy.postInit()
     }
 }
