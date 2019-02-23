b9a86ebc21a12e7fdd953b331a6e247d45062247
Cydhra <ubezl@student.kit.edu>
2019-02-24 00:17:51 +0100

    ingots are in creative tab materials

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/items/IngotItem.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/items/IngotItem.kt
@@ -1,7 +1,14 @@
 package net.cydhra.technocracy.foundation.items
 
+import net.minecraft.creativetab.CreativeTabs
+
 /**
  * Any additional ingots created by this mod are ultimately an instance of this class
  */
 open class IngotItem(ingotMaterialName: String, color: ConstantItemColor)
-    : BaseItem("ingot.$ingotMaterialName", itemColor = color)
+    : BaseItem("ingot.$ingotMaterialName", itemColor = color) {
+
+    init {
+        this.creativeTab = CreativeTabs.MATERIALS
+    }
+}
