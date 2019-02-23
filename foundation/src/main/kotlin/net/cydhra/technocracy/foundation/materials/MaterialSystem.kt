f8fe77598177eae1fceb693cd32ce1f2b9ea857f
Cydhra <ubezl@student.kit.edu>
2019-02-23 23:48:39 +0100

    material system assignes a constant item color

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/materials/MaterialSystem.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/materials/MaterialSystem.kt
@@ -1,18 +1,22 @@
 package net.cydhra.technocracy.foundation.materials
 
 import net.cydhra.technocracy.foundation.blocks.OreBlock
+import net.cydhra.technocracy.foundation.items.ConstantItemColor
 import net.cydhra.technocracy.foundation.items.IngotItem
 import net.cydhra.technocracy.foundation.items.ItemManager
 
 /**
  * A material system consists of an ore type, an ingot and all stages of ore processing necessary.
+ *
+ * @param materialName name of the mineable material this system introduces
+ * @param colorMultiplier the color of the material states
  */
-class MaterialSystem(materialName: String) {
+class MaterialSystem(materialName: String, colorMultiplier: Int) {
 
     /**
      * The ingot of this material
      */
-    val ingot = IngotItem(materialName)
+    val ingot = IngotItem(materialName, ConstantItemColor(colorMultiplier))
 
     /**
      * The ore of this material
