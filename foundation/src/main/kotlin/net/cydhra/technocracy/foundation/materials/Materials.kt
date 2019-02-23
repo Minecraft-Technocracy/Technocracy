111c15d4f0522e5c3e6e3ae7dff12655c8d7d660
Cydhra <ubezl@student.kit.edu>
2019-02-24 00:49:27 +0100

    added common material systems and registered them

---
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/materials/Materials.kt
@@ -0,0 +1,9 @@
+package net.cydhra.technocracy.foundation.materials
+
+val aluminiumSystem = MaterialSystem("aluminium", 0xFFFFFF)
+val copperSystem = MaterialSystem("copper", 0xF78725)
+val leadSystem = MaterialSystem("lead", 0x9696BE)
+val nickelSystem = MaterialSystem("nickel", 0xB56932)
+val osmiumSystem = MaterialSystem("osmium", 0x4682B4)
+val silverSystem = MaterialSystem("silver", 0xE0E0FF)
+val tinSystem = MaterialSystem("tin", 0xC6C6C6)
--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/proxy/CommonProxy.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/proxy/CommonProxy.kt
@@ -1,9 +1,12 @@
 package net.cydhra.technocracy.foundation.proxy
 
+import net.cydhra.technocracy.foundation.materials.*
+
 open class CommonProxy : ISidedProxy {
 
     override fun preInit() {
-
+        arrayOf(aluminiumSystem, copperSystem, leadSystem, nickelSystem, osmiumSystem, silverSystem, tinSystem)
+                .forEach(MaterialSystem::register)
     }
 
     override fun init() {
