94a261baf81c1581d83d8dd446e221f28ee0f407
Cydhra <ubezl@student.kit.edu>
2019-02-24 01:31:41 +0100

    added lithium system

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/materials/Materials.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/materials/Materials.kt
@@ -3,6 +3,7 @@ package net.cydhra.technocracy.foundation.materials
 val aluminiumSystem = MaterialSystem("aluminium", 0xFFFFFF)
 val copperSystem = MaterialSystem("copper", 0xF78725)
 val leadSystem = MaterialSystem("lead", 0x9696BE)
+val lithiumSystem = MaterialSystem("lithium", 0x176B59)
 val nickelSystem = MaterialSystem("nickel", 0xB56932)
 val osmiumSystem = MaterialSystem("osmium", 0x4682B4)
 val silverSystem = MaterialSystem("silver", 0xE0E0FF)
--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/proxy/CommonProxy.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/proxy/CommonProxy.kt
@@ -5,7 +5,8 @@ import net.cydhra.technocracy.foundation.materials.*
 open class CommonProxy : ISidedProxy {
 
     override fun preInit() {
-        arrayOf(aluminiumSystem, copperSystem, leadSystem, nickelSystem, osmiumSystem, silverSystem, tinSystem)
+        arrayOf(aluminiumSystem, copperSystem, leadSystem,
+                lithiumSystem, nickelSystem, osmiumSystem, silverSystem, tinSystem)
                 .forEach(MaterialSystem::register)
     }
 
