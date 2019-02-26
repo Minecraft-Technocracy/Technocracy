a8e98501f9fa0af388a524286f421e419e81a025
Cydhra <ubezl@student.kit.edu>
2019-02-26 17:13:42 +0100

    implemented ConstantBlockColor

---
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/ConstantBlockColor.kt
@@ -0,0 +1,16 @@
+package net.cydhra.technocracy.foundation.blocks
+
+import net.minecraft.block.state.IBlockState
+import net.minecraft.client.renderer.color.IBlockColor
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.IBlockAccess
+
+/**
+ * Implementation of [IBlockColor] that does return the same color in any case. Used for tinting blocks independent
+ * of state and location
+ */
+class ConstantBlockColor(private val constantItemColor: Int) : IBlockColor {
+    override fun colorMultiplier(state: IBlockState, worldIn: IBlockAccess?, pos: BlockPos?, tintIndex: Int): Int {
+        return this.constantItemColor
+    }
+}
