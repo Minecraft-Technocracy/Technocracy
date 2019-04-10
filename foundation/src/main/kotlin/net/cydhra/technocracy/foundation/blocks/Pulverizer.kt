3ea691e9a9feba979bc34dcde4da02c211b04272
Cydhra <ubezl@student.kit.edu>
2019-04-11 00:34:22 +0200

    configured pulverizer block stats

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/Pulverizer.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/Pulverizer.kt
@@ -3,11 +3,18 @@ package net.cydhra.technocracy.foundation.blocks
 import net.cydhra.technocracy.foundation.blocks.general.BaseTileEntityBlock
 import net.cydhra.technocracy.foundation.tileentity.TileEntityPulveriser
 import net.minecraft.block.material.Material
+import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.world.World
 
 
-class Pulverizer : BaseTileEntityBlock("pulverizer", material = Material.IRON) {
+class Pulverizer : BaseTileEntityBlock("pulverizer", material = Material.ROCK) {
+
+    init {
+        this.setCreativeTab(CreativeTabs.MATERIALS)
+        this.setHardness(2f)
+        this.setResistance(4f)
+    }
 
     override fun createNewTileEntity(worldIn: World, meta: Int): TileEntity? {
         return TileEntityPulveriser()
