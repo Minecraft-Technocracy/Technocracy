4c3e990bd1237fe9950cffd330aa1e1ebc2d6401
Cydhra <ubezl@student.kit.edu>
2019-04-11 00:17:29 +0200

    implemented most oil sand mechanics

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/OilSand.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/OilSand.kt
@@ -1,12 +1,25 @@
 package net.cydhra.technocracy.foundation.blocks
 
+import net.cydhra.technocracy.foundation.blocks.color.ConstantBlockColor
 import net.cydhra.technocracy.foundation.blocks.general.AbstractFallingBaseBlock
 import net.minecraft.block.SoundType
 import net.minecraft.block.material.Material
 import net.minecraft.block.state.IBlockState
 import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.entity.item.EntityFallingBlock
+import net.minecraft.init.Blocks
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.util.BlockRenderLayer
+import net.minecraft.util.NonNullList
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.IBlockAccess
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import java.util.*
 
-class OilSand : AbstractFallingBaseBlock("oil_sand", material = Material.SAND) {
+class OilSand : AbstractFallingBaseBlock("oil_sand", colorMultiplier = ConstantBlockColor(0x1C1004),
+        material = Material.SAND) {
 
     init {
         this.soundType = SoundType.SAND
@@ -18,5 +31,27 @@ class OilSand : AbstractFallingBaseBlock("oil_sand", material = Material.SAND) {
 
     override fun isToolEffective(type: String, state: IBlockState): Boolean {
         return type == "shovel"
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun getBlockLayer(): BlockRenderLayer {
+        return BlockRenderLayer.CUTOUT
+    }
+
+    override fun getDrops(drops: NonNullList<ItemStack>, world: IBlockAccess, pos: BlockPos, state: IBlockState, fortune: Int) {
+        drops.add(ItemStack(Blocks.SAND))
+    }
+
+    override fun getSilkTouchDrop(state: IBlockState): ItemStack {
+        return ItemStack(Blocks.SAND)
+    }
+
+    override fun getItemDropped(state: IBlockState, rand: Random, fortune: Int): Item {
+        return Blocks.SAND.getItemDropped(state, rand, fortune)
+    }
+
+    override fun onStartFalling(fallingEntity: EntityFallingBlock) {
+        fallingEntity.shouldDropItem = false
+        super.onStartFalling(fallingEntity)
     }
 }
