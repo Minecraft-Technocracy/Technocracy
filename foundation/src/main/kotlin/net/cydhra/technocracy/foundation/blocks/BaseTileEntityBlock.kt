1d96f0af6fcdc1308bd8509019dbd7a625072b94
Poke <florian.strobel@yahoo.de>
2019-04-10 22:39:17 +0200

    rotateable Pulverizer

---
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/BaseTileEntityBlock.kt
@@ -0,0 +1,57 @@
+package net.cydhra.technocracy.foundation.blocks
+
+import net.cydhra.technocracy.foundation.blocks.color.ConstantBlockColor
+import net.minecraft.block.BlockHorizontal
+import net.minecraft.block.ITileEntityProvider
+import net.minecraft.block.material.Material
+import net.minecraft.block.properties.IProperty
+import net.minecraft.block.state.BlockStateContainer
+import net.minecraft.block.state.IBlockState
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.EnumHand
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+
+abstract class BaseTileEntityBlock : BaseBlock , ITileEntityProvider {
+
+    companion object {
+        val FACING = BlockHorizontal.FACING;
+    }
+
+    constructor(unlocalizedName: String, registryName: String, colorMultiplier: ConstantBlockColor?, material: Material) : super(unlocalizedName, registryName, colorMultiplier, material)
+    constructor(unlocalizedName: String, material: Material) : super(unlocalizedName, material = material)
+
+    init {
+        defaultState = this.blockState.baseState.withProperty(FACING, EnumFacing.NORTH)
+    }
+
+    override fun onBlockActivated(worldIn: World, pos: BlockPos, state: IBlockState, playerIn: EntityPlayer, hand: EnumHand, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float): Boolean {
+        if(worldIn.isRemote) {
+            //TODO open gui
+            //playerIn.openGui()
+        }
+        return true
+    }
+
+    override fun createBlockState(): BlockStateContainer {
+        return BlockStateContainer(this, FACING)
+    }
+
+    override fun getStateFromMeta(meta: Int): IBlockState {
+        var facing = EnumFacing.getFront(meta)
+        if (facing.axis == EnumFacing.Axis.Y) facing = EnumFacing.NORTH
+        return this.defaultState.withProperty(FACING, facing)
+    }
+
+    override fun getMetaFromState(state: IBlockState): Int {
+        return (state.getValue(FACING) as EnumFacing).index
+    }
+
+    override fun onBlockPlacedBy(worldIn: World, pos: BlockPos, state: IBlockState, placer: EntityLivingBase, stack: ItemStack) {
+        worldIn.setBlockState(pos, state.withProperty(FACING, placer.adjustedHorizontalFacing.opposite))
+    }
+}
--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/Pulverizer.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/Pulverizer.kt
@@ -1,19 +1,13 @@
 package net.cydhra.technocracy.foundation.blocks
 
 import net.cydhra.technocracy.foundation.tileentity.TileEntityPulveriser
-import net.minecraft.block.ITileEntityProvider
 import net.minecraft.block.material.Material
-import net.minecraft.block.state.IBlockState
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.world.World
 
 
-class Pulverizer : BaseBlock("pulverizer", material = Material.IRON), ITileEntityProvider {
+class Pulverizer : BaseTileEntityBlock("pulverizer", material = Material.IRON) {
     override fun createNewTileEntity(worldIn: World, meta: Int): TileEntity? {
-        return TileEntityPulveriser()
-    }
-
-    override fun createTileEntity(world: World, state: IBlockState): TileEntity? {
         return TileEntityPulveriser()
     }
 }
