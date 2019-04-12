174e5b7fd4d5cba1087ab0d988eb1872c6da659e
Cydhra <ubezl@student.kit.edu>
2019-04-13 01:15:15 +0200

    fixed broken imports of merge conflict

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/AbstractMachine.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/AbstractMachine.kt
@@ -1,7 +1,5 @@
 package net.cydhra.technocracy.foundation.tileentity
 
-import net.cydhra.technocracy.foundation.TCFoundation
-import net.cydhra.technocracy.foundation.client.renderer.tileEntity.TileEntityElectricFurnaceRenderer
 import net.cydhra.technocracy.foundation.tileentity.components.*
 import net.minecraft.block.state.IBlockState
 import net.minecraft.nbt.NBTTagCompound
@@ -9,7 +7,6 @@ import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.ITickable
 import net.minecraftforge.common.capabilities.Capability
-import net.minecraftforge.fml.client.registry.ClientRegistry
 
 /**
  * Base class for all machine [TileEntities][TileEntity]. The tile entity automatically has capabilities for energy
