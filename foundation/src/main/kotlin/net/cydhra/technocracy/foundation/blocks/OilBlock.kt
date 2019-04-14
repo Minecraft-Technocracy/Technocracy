be08a48d7adae84dab605868a40367ed7c75fa7c
Cydhra <ubezl@student.kit.edu>
2019-04-14 19:52:45 +0200

    temporarily registered other state mapper until original state mapper is fixed

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/OilBlock.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/OilBlock.kt
@@ -1,29 +1,38 @@
 package net.cydhra.technocracy.foundation.blocks
 
+import net.cydhra.technocracy.foundation.TCFoundation
 import net.cydhra.technocracy.foundation.blocks.general.AbstractBaseLiquid
 import net.cydhra.technocracy.foundation.liquids.general.oil
 import net.minecraft.block.material.Material
 import net.minecraft.block.state.IBlockState
+import net.minecraft.client.renderer.ItemMeshDefinition
+import net.minecraft.client.renderer.block.model.ModelResourceLocation
+import net.minecraft.client.renderer.block.statemap.StateMapperBase
+import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumBlockRenderType
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.IBlockAccess
+import net.minecraftforge.client.model.ModelLoader
 
 
-class OilBlock : AbstractBaseLiquid(oil, "oil", Material.WATER) {
+class OilBlock : AbstractBaseLiquid(oil, "oil_block", Material.WATER) {
+
+    val location: ModelResourceLocation = ModelResourceLocation(TCFoundation.MODID + ":" + "fluid", "oil")
 
     init {
         fluid.block = this
-    }
-
-    override fun onNeighborChange(world: IBlockAccess, pos: BlockPos, neighbor: BlockPos) {
-
-        val changed = world.getBlockState(neighbor)
-
-
-        super.onNeighborChange(world, pos, neighbor)
+        ModelLoader.setCustomStateMapper(this, CustomStateMapper())
     }
 
     override fun getRenderType(state: IBlockState): EnumBlockRenderType {
         return EnumBlockRenderType.MODEL
+    }
+
+    inner class CustomStateMapper : StateMapperBase(), ItemMeshDefinition {
+        override fun getModelResourceLocation(state: IBlockState): ModelResourceLocation {
+            return location
+        }
+
+        override fun getModelLocation(stack: ItemStack): ModelResourceLocation {
+            return location
+        }
     }
 }
