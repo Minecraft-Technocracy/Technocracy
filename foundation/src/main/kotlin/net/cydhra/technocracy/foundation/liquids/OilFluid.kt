be141117d1a40fee899ac520946afe728bcb545e
Cydhra <ubezl@student.kit.edu>
2019-04-14 19:52:02 +0200

    fixed path to test textures

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/liquids/OilFluid.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/liquids/OilFluid.kt
@@ -2,13 +2,20 @@ package net.cydhra.technocracy.foundation.liquids
 
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fluids.Fluid
+import net.minecraftforge.fluids.FluidStack
 import java.awt.Color
 
 
-class OilFluid : Fluid("oil", ResourceLocation("technocracy.foundation", "steel"), ResourceLocation("technocracy" +
-        ".foundation", "steel_dark"), Color(46,46,46)) {
+class OilFluid : Fluid("oil", ResourceLocation("technocracy.foundation", "block/steel"),
+        ResourceLocation("technocracy.foundation", "block/steel_dark"), Color(46, 46, 46)) {
 
     init {
-        //setBlock(oilSandBlock)
+
     }
+
+    override fun doesVaporize(fluidStack: FluidStack?): Boolean {
+        return false
+    }
+
+
 }
