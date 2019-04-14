901fa85e53eb593a13aae0dd520ab8fbcfd17fe6
Cydhra <ubezl@student.kit.edu>
2019-04-14 22:16:04 +0200

    increased oil viscosity

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/liquids/general/Fluids.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/liquids/general/Fluids.kt
@@ -3,4 +3,4 @@ package net.cydhra.technocracy.foundation.liquids.general
 import java.awt.Color
 
 
-val oilFluid = BaseFluid("oil", Color(25, 25, 25), opaqueTexture = true).setViscosity(2000).setDensity(900)
+val oilFluid = BaseFluid("oil", Color(25, 25, 25), opaqueTexture = true).setViscosity(3000).setDensity(900)
