ada21aa39125045dc2a0cd7500755152e0878e2b
Cydhra <ubezl@student.kit.edu>
2019-04-15 00:54:59 +0200

    defined oil material. Oil is no longer considered liquid by entities

---
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/material/Materials.kt
@@ -0,0 +1,6 @@
+package net.cydhra.technocracy.foundation.blocks.material
+
+import net.minecraft.block.material.MapColor
+import net.minecraft.block.material.MaterialLiquid
+
+val oilMaterial = MaterialLiquid(MapColor.BLACK_STAINED_HARDENED_CLAY)
