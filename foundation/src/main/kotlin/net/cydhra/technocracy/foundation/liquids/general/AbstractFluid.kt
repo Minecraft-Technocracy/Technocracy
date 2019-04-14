011d0a36bcc1b915870b20c1e67074adac1570df
Cydhra <ubezl@student.kit.edu>
2019-04-14 21:58:10 +0200

    doc for AbstractFluid

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/liquids/general/AbstractFluid.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/liquids/general/AbstractFluid.kt
@@ -4,7 +4,14 @@ import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fluids.Fluid
 import java.awt.Color
 
-
+/**
+ * Abstract base class for all fluids added by this modification. They automatically get fluid textures assigned
+ * which are manipulated through a color
+ *
+ * @param fluidName unlocalized and registry name for fluid
+ * @param color color modifier for fluid textures (also for universal bucket texture)
+ * @param opaqueTexture whether to use the opaque or the transparent fluid texture
+ */
 abstract class AbstractFluid(fluidName: String, color: Color, opaqueTexture: Boolean) : Fluid(fluidName,
         ResourceLocation("technocracy.foundation", "liquid/fluid_${if (opaqueTexture) "opaque_" else ""}still"),
         ResourceLocation("technocracy.foundation", "liquid/fluid_${if (opaqueTexture) "opaque_" else ""}flow"),
