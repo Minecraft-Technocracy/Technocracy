9f1f731caef22491a8addde2b2b3f61e6c8b3373
Cydhra <ubezl@student.kit.edu>
2019-04-14 18:15:15 +0200

    code style

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/general/AbstractBaseLiquid.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/general/AbstractBaseLiquid.kt
@@ -6,7 +6,13 @@ import net.minecraft.block.material.Material
 import net.minecraftforge.fluids.BlockFluidClassic
 import net.minecraftforge.fluids.Fluid
 
-abstract class AbstractBaseLiquid(fluid: Fluid, unlocalizedName: String, material: Material, registryName: String = unlocalizedName, mapColor: MapColor = material.materialMapColor, override val colorMultiplier: ConstantBlockColor? = null) : BlockFluidClassic(fluid, material, mapColor), IBaseBlock {
+abstract class AbstractBaseLiquid(fluid: Fluid,
+                                  unlocalizedName: String,
+                                  material: Material,
+                                  registryName: String = unlocalizedName,
+                                  mapColor: MapColor = material.materialMapColor,
+                                  override val colorMultiplier: ConstantBlockColor? = null)
+    : BlockFluidClassic(fluid, material, mapColor), IBaseBlock {
 
     override val modelLocation: String
         get() = this.registryName.toString()
