ec337794a812d2cf701f4fad26fffbb1412741c8
Cydhra <ubezl@student.kit.edu>
2019-05-02 19:30:19 +0200

    "implemented" MachineTileEntity

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/MachineTileEntity.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/MachineTileEntity.kt
@@ -1,10 +1,44 @@
 package net.cydhra.technocracy.foundation.tileentity
 
+import net.cydhra.technocracy.foundation.client.gui.machine.Tab
+import net.cydhra.technocracy.foundation.client.gui.machine.tabs.WipTab
+import net.cydhra.technocracy.foundation.tileentity.components.EnergyStorageComponent
+import net.cydhra.technocracy.foundation.tileentity.components.MachineUpgradesComponents
+import net.cydhra.technocracy.foundation.tileentity.components.RedstoneModeComponent
 import net.cydhra.technocracy.foundation.tileentity.logic.ILogicClient
 import net.cydhra.technocracy.foundation.tileentity.logic.LogicClientDelegate
+import net.minecraft.util.EnumFacing
 
 open class MachineTileEntity : AggregatableTileEntity(), TCMachineTileEntity, ILogicClient by LogicClientDelegate() {
+    /**
+     * The machine's redstone mode
+     */
+    protected val redstoneModeComponent = RedstoneModeComponent()
+
+    /**
+     * The machines internal energy storage and transfer limit state
+     */
+    protected val energyStorageComponent = EnergyStorageComponent(mutableSetOf(EnumFacing.UP))
+
+    /**
+     * The machine upgrades component.
+     */
+    /* TODO as the possible upgrades are dependant of machine type, either split this compound into single upgrades or
+        at least handle it from subclass*/
+    protected val machineUpgradesComponent = MachineUpgradesComponents()
+
+    init {
+        this.registerComponent(redstoneModeComponent, "redstone_mode")
+        this.registerComponent(energyStorageComponent, "energy")
+        this.registerComponent(machineUpgradesComponent, "upgrades")
+    }
+
+    fun getGuiTabs(): Array<Tab> {
+        return arrayOf(WipTab(500, 500))
+    }
+
     override fun update() {
-        TODO("not implemented")
+        // update ILogic strategies
+        this.tick()
     }
 }
