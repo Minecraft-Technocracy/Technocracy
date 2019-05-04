812746628f9c111261e907a4e5cf104eee4baad9
Cydhra <ubezl@student.kit.edu>
2019-05-04 17:48:31 +0200

    removed todos in BoilerMultiBlock so it can be tested

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/multiblock/BoilerMultiBlock.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/multiblock/BoilerMultiBlock.kt
@@ -3,73 +3,76 @@ package net.cydhra.technocracy.foundation.multiblock
 import it.zerono.mods.zerocore.api.multiblock.IMultiblockPart
 import it.zerono.mods.zerocore.api.multiblock.MultiblockControllerBase
 import it.zerono.mods.zerocore.lib.block.ModTileEntity
+import net.cydhra.technocracy.foundation.blocks.general.boilerControllerBlock
+import net.cydhra.technocracy.foundation.blocks.general.boilerWallBlock
+import net.minecraft.init.Blocks
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.world.World
 import java.util.function.Predicate
 
 class BoilerMultiBlock(world: World) : BaseMultiBlock(
-        frameBlockWhitelist = Predicate { true },
-        sideBlockWhitelist = Predicate { true },
-        topBlockWhitelist = Predicate { true },
-        bottomBlockWhitelist = Predicate { true },
-        interiorBlockWhitelist = Predicate { true },
+        frameBlockWhitelist = Predicate { it.block == boilerWallBlock || it.block == boilerControllerBlock },
+        sideBlockWhitelist = Predicate { it.block == boilerWallBlock || it.block == boilerControllerBlock },
+        topBlockWhitelist = Predicate { it.block == boilerWallBlock },
+        bottomBlockWhitelist = Predicate { it.block == boilerWallBlock },
+        interiorBlockWhitelist = Predicate { it.block == Blocks.AIR },
         maximumSizeXZ = 16,
         maximumSizeY = 16,
         world = world) {
 
     override fun updateServer(): Boolean {
-        TODO("not implemented")
-    }
-
-    override fun getMinimumNumberOfBlocksForAssembledMachine(): Int {
-        TODO("not implemented")
-    }
-
-    override fun onBlockAdded(p0: IMultiblockPart?) {
-        TODO("not implemented")
+        return true
     }
 
     override fun updateClient() {
-        TODO("not implemented")
+
+    }
+
+    override fun onBlockAdded(p0: IMultiblockPart?) {
+
     }
 
     override fun onBlockRemoved(p0: IMultiblockPart?) {
-        TODO("not implemented")
+
     }
 
     override fun onAssimilate(p0: MultiblockControllerBase?) {
-        TODO("not implemented")
+
     }
 
     override fun onAttachedPartWithMultiblockData(p0: IMultiblockPart?, p1: NBTTagCompound?) {
-        TODO("not implemented")
+
     }
 
     override fun onMachineAssembled() {
-        TODO("not implemented")
+
     }
 
     override fun syncDataFrom(p0: NBTTagCompound?, p1: ModTileEntity.SyncReason?) {
-        TODO("not implemented")
+
     }
 
     override fun onAssimilated(p0: MultiblockControllerBase?) {
-        TODO("not implemented")
+
     }
 
     override fun onMachineRestored() {
-        TODO("not implemented")
+
     }
 
     override fun syncDataTo(p0: NBTTagCompound?, p1: ModTileEntity.SyncReason?) {
-        TODO("not implemented")
+
     }
 
     override fun onMachinePaused() {
-        TODO("not implemented")
+
     }
 
     override fun onMachineDisassembled() {
-        TODO("not implemented")
+
+    }
+
+    override fun getMinimumNumberOfBlocksForAssembledMachine(): Int {
+        return 26
     }
 }
