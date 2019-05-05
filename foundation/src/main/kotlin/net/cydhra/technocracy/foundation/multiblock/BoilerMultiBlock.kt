449b9231b1c590f7dc41be476c0200ced8c5dbad
Cydhra <ubezl@student.kit.edu>
2019-05-05 19:21:44 +0200

    boiler checks that only one controller is present

--- foundation/src/main/kotlin/net/cydhra/technocracy/foundation/multiblock/BoilerMultiBlock.kt
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/multiblock/BoilerMultiBlock.kt
@@ -2,9 +2,11 @@ package net.cydhra.technocracy.foundation.multiblock
 
 import it.zerono.mods.zerocore.api.multiblock.IMultiblockPart
 import it.zerono.mods.zerocore.api.multiblock.MultiblockControllerBase
+import it.zerono.mods.zerocore.api.multiblock.validation.IMultiblockValidator
 import it.zerono.mods.zerocore.lib.block.ModTileEntity
 import net.cydhra.technocracy.foundation.blocks.general.boilerControllerBlock
 import net.cydhra.technocracy.foundation.blocks.general.boilerWallBlock
+import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityBoilerController
 import net.minecraft.init.Blocks
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.world.World
@@ -20,12 +22,40 @@ class BoilerMultiBlock(world: World) : BaseMultiBlock(
         maximumSizeY = 16,
         world = world) {
 
+    private var controllerTileEntity: TileEntityBoilerController? = null
+
     override fun updateServer(): Boolean {
         return true
     }
 
     override fun updateClient() {
 
+    }
+
+    override fun isMachineWhole(validatorCallback: IMultiblockValidator): Boolean {
+        if (!super.isMachineWhole(validatorCallback))
+            return false
+
+        var controllerTileEntity: TileEntityBoilerController? = null
+
+        for (part in this.connectedParts) {
+            if (part is TileEntityBoilerController) {
+                if (controllerTileEntity == null)
+                    controllerTileEntity = part
+                else {
+                    validatorCallback.setLastError("only one controller is allowed")
+                    return false
+                }
+            }
+        }
+
+        if (controllerTileEntity == null) {
+            validatorCallback.setLastError("controller is missing")
+            return false
+        }
+
+        this.controllerTileEntity = controllerTileEntity
+        return true
     }
 
     override fun onBlockAdded(p0: IMultiblockPart?) {
