b5d770a78bfbb7944ede524540866c194a7742b1
Cydhra <ubezl@student.kit.edu>
2019-05-05 18:53:45 +0200

    created a convenience class for creating non-behaviour multi-block parts

---
+++ foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/general/PlainMultiBlockPartBlock.kt
@@ -0,0 +1,18 @@
+package net.cydhra.technocracy.foundation.blocks.general
+
+import it.zerono.mods.zerocore.api.multiblock.IMultiblockPart
+import net.cydhra.technocracy.foundation.blocks.api.TCMultiBlock
+import net.cydhra.technocracy.foundation.tileentity.api.TCControllerTileEntity
+import net.minecraft.block.material.Material
+import net.minecraft.tileentity.TileEntity
+
+/**
+ * A plain, non-rotatable block that creates a multiblock tile entity when placed, using the given constructor
+ *
+ * @param unlocalizedName registry and language name of the block
+ * @param tileEntityConstructor constructor for the tile entity that is created
+ */
+class PlainMultiBlockPartBlock<T>(unlocalizedName: String, tileEntityConstructor: () -> T)
+    : AbstractBaseBlock(unlocalizedName, Material.IRON),
+        TCMultiBlock<T> by MultiBlockBaseDelegate<T>(tileEntityConstructor)
+        where T : TileEntity, T : TCControllerTileEntity, T : IMultiblockPart
