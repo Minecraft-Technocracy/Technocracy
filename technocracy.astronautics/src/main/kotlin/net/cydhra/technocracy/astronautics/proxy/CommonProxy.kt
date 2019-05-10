f33672e01d35e4eccfe11e99d187926a5e33b3b6
Cydhra <ubezl@student.kit.edu>
2019-05-10 19:14:06 +0200

    implemented orbit provider

--- technocracy.astronautics/src/main/kotlin/net/cydhra/technocracy/astronautics/proxy/CommonProxy.kt
+++ technocracy.astronautics/src/main/kotlin/net/cydhra/technocracy/astronautics/proxy/CommonProxy.kt
@@ -1,5 +1,11 @@
 package net.cydhra.technocracy.astronautics.proxy
 
+import net.cydhra.technocracy.astronautics.capabilities.satellites.DefaultSatelliteOrbit
+import net.cydhra.technocracy.astronautics.capabilities.satellites.ISatelliteOrbit
+import net.cydhra.technocracy.astronautics.capabilities.satellites.SatelliteOrbitStorage
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.common.capabilities.CapabilityManager
+
 open class CommonProxy {
 
     open fun preInit() {
@@ -7,7 +13,9 @@ open class CommonProxy {
     }
 
     open fun init() {
-
+        CapabilityManager.INSTANCE.register(ISatelliteOrbit::class.java, SatelliteOrbitStorage(),
+                ::DefaultSatelliteOrbit)
+        MinecraftForge.EVENT_BUS.register(CapabilityEventBusHandler())
     }
 
     open fun postInit() {
