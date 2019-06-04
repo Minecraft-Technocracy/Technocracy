fa6d3316dff41df11f4daa9fa9704df417b93a4a
Flaflo <flaflo@ok.de>
2019-06-04 23:41:13 +0200

    Renamed variable for naming conventions Added RenderWorldEvent for testing

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/proxy/RenderEventListener.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/proxy/RenderEventListener.kt
@@ -15,8 +15,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @Mod.EventBusSubscriber(modid = TCFoundation.MODID)
 class RenderEventListener {
-    private val UNDERWATER_GRAYSCALE = ResourceLocation("technocracy.foundation",
-            "textures/extra/underwater_grayscale.png")
+    private val underwaterGrayscale =
+            ResourceLocation("technocracy.foundation", "textures/extra/underwater_grayscale.png")
 
     /*@SubscribeEvent
     fun handleFogDensityEvent(event: EntityViewRenderEvent.FogDensity) {
@@ -44,60 +44,68 @@ class RenderEventListener {
                 //event.blue = 5/255f
             }
         }
-    }
-
-    @SubscribeEvent
-    fun overlay(event: RenderHandEvent) {
-        val mc = Minecraft.getMinecraft()
-            val player = mc.player
-
-        val playerEyePos = player.getPositionEyes(event.partialTicks)
-        val pos = BlockPos(playerEyePos)
-        val state = player.world.getBlockState(pos)
-        val block = state.block
-
-            if(player.isPotionActive(oilyEffect) && !state.material.isLiquid) {
-
-                event.isCanceled = true
-                val flag = mc.renderViewEntity is EntityLivingBase && (mc.renderViewEntity as
-                        EntityLivingBase).isPlayerSleeping
-
-                if (mc.gameSettings.thirdPersonView == 0 && !flag && !mc.gameSettings.hideGUI &&
-                        !mc.playerController.isSpectator()) {
-
-                    GlStateManager.translate(0.0,0.0,+0.1)
-                    mc.entityRenderer.enableLightmap()
-                    mc.entityRenderer.itemRenderer.renderItemInFirstPerson(event.partialTicks)
-                    mc.entityRenderer.disableLightmap()
-                }
-
-                GlStateManager.popMatrix()
-                GlStateManager.pushMatrix()
-
-                Minecraft.getMinecraft().renderEngine.bindTexture(UNDERWATER_GRAYSCALE_1)
-                GlStateManager.color(0.0f, 0.0f, 0.0f, 1f)
-                GlStateManager.enableBlend()
-                GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO)
-                GlStateManager.pushMatrix()
-
-                val yaw = -player.rotationYaw / 64.0f
-                val pitch = player.rotationPitch / 64.0f
-
-                val t = Tessellator.getInstance()
-                val buffer = t.buffer
-
-                buffer.begin(0x07, DefaultVertexFormats.POSITION_TEX)
-                buffer.pos(-1.0, -1.0, -0.5).tex((4.0f + yaw).toDouble(), (4.0f + pitch).toDouble()).endVertex()
-                buffer.pos(1.0, -1.0, -0.5).tex((0.0f + yaw).toDouble(), (4.0f + pitch).toDouble()).endVertex()
-                buffer.pos(1.0, 1.0, -0.5).tex((0.0f + yaw).toDouble(), (0.0f + pitch).toDouble()).endVertex()
-                buffer.pos(-1.0, 1.0, -0.5).tex((4.0f + yaw).toDouble(), (0.0f + pitch).toDouble()).endVertex()
-                t.draw()
-
-                GlStateManager.popMatrix()
-                GlStateManager.disableBlend()
-                GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
-        }
     }*/
+
+//    @SubscribeEvent
+//    fun onRenderWorld(event: RenderWorldLastEvent) {
+//        GlStateManager.pushMatrix()
+//
+//        GlStateManager.popMatrix()
+//    }
+
+//    @SubscribeEvent
+//    fun overlay(event: RenderHandEvent) {
+
+//        val mc = Minecraft.getMinecraft()
+//            val player = mc.player
+//
+//        val playerEyePos = player.getPositionEyes(event.partialTicks)
+//        val pos = BlockPos(playerEyePos)
+//        val state = player.world.getBlockState(pos)
+//        val block = state.block
+//
+//            if(player.isPotionActive(oilyEffect) && !state.material.isLiquid) {
+//
+//                event.isCanceled = true
+//                val flag = mc.renderViewEntity is EntityLivingBase && (mc.renderViewEntity as
+//                        EntityLivingBase).isPlayerSleeping
+//
+//                if (mc.gameSettings.thirdPersonView == 0 && !flag && !mc.gameSettings.hideGUI &&
+//                        !mc.playerController.isSpectator()) {
+//
+//                    GlStateManager.translate(0.0,0.0,+0.1)
+//                    mc.entityRenderer.enableLightmap()
+//                    mc.entityRenderer.itemRenderer.renderItemInFirstPerson(event.partialTicks)
+//                    mc.entityRenderer.disableLightmap()
+//                }
+//
+//                GlStateManager.popMatrix()
+//                GlStateManager.pushMatrix()
+//
+//                Minecraft.getMinecraft().renderEngine.bindTexture(UNDERWATER_GRAYSCALE_1)
+//                GlStateManager.color(0.0f, 0.0f, 0.0f, 1f)
+//                GlStateManager.enableBlend()
+//                GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO)
+//                GlStateManager.pushMatrix()
+//
+//                val yaw = -player.rotationYaw / 64.0f
+//                val pitch = player.rotationPitch / 64.0f
+//
+//                val t = Tessellator.getInstance()
+//                val buffer = t.buffer
+//
+//                buffer.begin(0x07, DefaultVertexFormats.POSITION_TEX)
+//                buffer.pos(-1.0, -1.0, -0.5).tex((4.0f + yaw).toDouble(), (4.0f + pitch).toDouble()).endVertex()
+//                buffer.pos(1.0, -1.0, -0.5).tex((0.0f + yaw).toDouble(), (4.0f + pitch).toDouble()).endVertex()
+//                buffer.pos(1.0, 1.0, -0.5).tex((0.0f + yaw).toDouble(), (0.0f + pitch).toDouble()).endVertex()
+//                buffer.pos(-1.0, 1.0, -0.5).tex((4.0f + yaw).toDouble(), (0.0f + pitch).toDouble()).endVertex()
+//                t.draw()
+//
+//                GlStateManager.popMatrix()
+//                GlStateManager.disableBlend()
+//                GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
+//        }
+//    }
 
     @SubscribeEvent
     @Suppress("unused")
@@ -111,13 +119,17 @@ class RenderEventListener {
             val block = state.block
 
             if (block is BlockFluidBase) {
-                Minecraft.getMinecraft().renderEngine.bindTexture(UNDERWATER_GRAYSCALE)
+                Minecraft.getMinecraft().renderEngine.bindTexture(underwaterGrayscale)
                 val brightness = 0.8
-                val color = block.getFogColor(player.world, pos, state, player, Vec3d(1.0, 1.0, 1.0), 0.0f).scale(brightness)
+                val color = block.getFogColor(player.world, pos, state, player, Vec3d(1.0, 1.0, 1.0), 0.0f)
+                        .scale(brightness)
 
                 GlStateManager.color(color.x.toFloat(), color.y.toFloat(), color.z.toFloat(), 0.4f)
                 GlStateManager.enableBlend()
-                GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO)
+                GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA,
+                        GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
+                        GlStateManager.SourceFactor.ONE,
+                        GlStateManager.DestFactor.ZERO)
                 GlStateManager.pushMatrix()
 
                 val yaw = -player.rotationYaw / 64.0f
