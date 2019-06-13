d03fafd47eb7384be89547f9697523dc3e3d234c
Poke <platin_one@yahoo.de>
2019-06-13 17:33:39 +0200

    Display stack for Facade creative tab is now randomized every 5 sekonds

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/client/CustomCreativeTab.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/client/CustomCreativeTab.kt
@@ -1,10 +1,11 @@
 package net.cydhra.technocracy.foundation.client
 
-import net.cydhra.technocracy.foundation.items.general.FacadeItem
 import net.cydhra.technocracy.foundation.items.general.facadeItem
 import net.cydhra.technocracy.foundation.materialsystems.niobiumSystem
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.ItemStack
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
 
 /**
  * Creative tab for all items of this mod. Since this are potentially many, a search bar is displayed
@@ -24,12 +25,25 @@ val technocracyCreativeTabs = object : CreativeTabs("technocracy") {
 }
 
 val technocracyFacadeCreativeTab = object : CreativeTabs("facades") {
-    init {
-        this.backgroundImageName = "items.png"
+    override fun getTabIconItem(): ItemStack {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getTabIconItem(): ItemStack {
-        return facadeItem.getRandomFacade()
+    lateinit var stack: ItemStack
+    var last: Long = -1
+
+    init {
+        this.backgroundImageName = "items.png"
+
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun getIconItemStack(): ItemStack {
+        if(last < System.currentTimeMillis()) {
+            last = System.currentTimeMillis() + 1000 * 5
+            stack = facadeItem.getRandomFacade()
+        }
+        return stack
     }
 
     override fun hasSearchBar(): Boolean {
