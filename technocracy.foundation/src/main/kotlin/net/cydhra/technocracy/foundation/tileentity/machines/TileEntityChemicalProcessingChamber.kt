3efdc95e135b2239a2df15c7d6a0007d0f65bfe7
Cydhra <ubezl@student.kit.edu>
2019-07-03 17:35:14 +0200

    item processing logic for most fluid-based machines

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/machines/TileEntityChemicalProcessingChamber.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/machines/TileEntityChemicalProcessingChamber.kt
@@ -1,8 +1,51 @@
 package net.cydhra.technocracy.foundation.tileentity.machines
 
+import net.cydhra.technocracy.foundation.capabilities.fluid.DynamicFluidHandler
+import net.cydhra.technocracy.foundation.capabilities.inventory.DynamicInventoryHandler
+import net.cydhra.technocracy.foundation.crafting.IMachineRecipe
+import net.cydhra.technocracy.foundation.crafting.RecipeManager
 import net.cydhra.technocracy.foundation.tileentity.MachineTileEntity
+import net.cydhra.technocracy.foundation.tileentity.components.FluidComponent
+import net.cydhra.technocracy.foundation.tileentity.components.InventoryComponent
+import net.cydhra.technocracy.foundation.tileentity.logic.ItemProcessingLogic
+import net.cydhra.technocracy.foundation.tileentity.management.TEInventoryProvider
+import net.minecraft.item.ItemStack
+import net.minecraft.util.EnumFacing
 
 /**
  *
  */
-class TileEntityChemicalProcessingChamber : MachineTileEntity()
+class TileEntityChemicalProcessingChamber : MachineTileEntity(), TEInventoryProvider {
+
+    private val inputInventoryComponent = InventoryComponent(1, this, EnumFacing.WEST)
+    private val inputFluidComponent = FluidComponent(4000,
+            tanktype = DynamicFluidHandler.TankType.INPUT, facing = mutableSetOf(EnumFacing.UP))
+    private val outputInventoryComponent = FluidComponent(4000,
+            tanktype = DynamicFluidHandler.TankType.OUTPUT, facing = mutableSetOf(EnumFacing.EAST))
+
+    private val recipes: Collection<IMachineRecipe> by lazy {
+        (RecipeManager.getRecipesByType(RecipeManager.RecipeType.CHEMICAL_PROCESSING) ?: emptyList())
+    }
+
+    init {
+        this.registerComponent(inputInventoryComponent, "input_inventory")
+        this.registerComponent(inputFluidComponent, "input_fluid")
+        this.registerComponent(outputInventoryComponent, "output_inventory")
+
+        this.addLogicStrategy(ItemProcessingLogic(
+                recipeType = RecipeManager.RecipeType.CHEMICAL_PROCESSING,
+                inputInventory = this.inputInventoryComponent.inventory,
+                inputFluidSlots = arrayOf(inputFluidComponent.fluid),
+                outputFluidSlots = arrayOf(this.outputInventoryComponent.fluid),
+                energyStorage = this.energyStorageComponent.energyStorage,
+                machineUpgrades = this.machineUpgradesComponent,
+                baseTickEnergyCost = 40
+        ))
+    }
+
+    override fun isItemValid(inventory: DynamicInventoryHandler, slot: Int, stack: ItemStack): Boolean {
+        return inventory == inputInventoryComponent.inventory && this.recipes.any { recipe ->
+            recipe.getInput().any { it.test(stack) }
+        }
+    }
+}
--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/machines/TileEntityChemicalReactionChamber.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/machines/TileEntityChemicalReactionChamber.kt
@@ -5,4 +5,6 @@ import net.cydhra.technocracy.foundation.tileentity.MachineTileEntity
 /**
  *
  */
-class TileEntityChemicalReactionChamber : MachineTileEntity()
+class TileEntityChemicalReactionChamber : MachineTileEntity() {
+
+}
