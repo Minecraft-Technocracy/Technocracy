d9a8a1c3a5fdd50b58022f1d4076cc6f6604cb64
Cydhra <ubezl@student.kit.edu>
2019-07-07 22:54:28 +0200

    removed heat storage from HE in favor of direct heat transmission

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/heatexchanger/TileEntityHeatExchangerController.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/heatexchanger/TileEntityHeatExchangerController.kt
@@ -1,41 +1,49 @@
 package net.cydhra.technocracy.foundation.tileentity.multiblock.heatexchanger
 
 import net.cydhra.technocracy.foundation.multiblock.HeatExchangerMultiBlock
-import net.cydhra.technocracy.foundation.tileentity.AggregatableDelegate
-import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatable
-import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatableTileEntity
-import net.cydhra.technocracy.foundation.tileentity.components.HeatStorageComponent
 import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityMultiBlockPart
 import net.minecraft.block.Block
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.text.TextComponentString
 import net.minecraft.world.World
 
-@Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class TileEntityHeatExchangerController : TileEntityMultiBlockPart<HeatExchangerMultiBlock>(HeatExchangerMultiBlock::class,
-        ::HeatExchangerMultiBlock), TCAggregatableTileEntity, TCAggregatable by AggregatableDelegate() {
+        ::HeatExchangerMultiBlock) {
 
-    private val heatStorageComponent = HeatStorageComponent(0)
+    companion object {
+        private const val NBT_KEY_MATRIX_COEFFICIENT = "matrix_efficiency"
+    }
+
+    private var matrixCoefficient = 0.0f
 
     init {
-        this.registerComponent(heatStorageComponent, "heat")
+
     }
 
     fun updatePhysics(matrixSize: Int, matrixType: Block?) {
-        this.heatStorageComponent.heatCapacity = matrixSize * 100
-        this.heatStorageComponent.drainEfficiency = getDrainEfficiencyForBlock(matrixType)
+        this.matrixCoefficient = getMatrixCoefficientForBlock(matrixType)
     }
 
-    private fun getDrainEfficiencyForBlock(block: Block?): Float {
+    private fun getMatrixCoefficientForBlock(block: Block?): Float {
         return 0.5f
     }
 
     override fun onActivate(world: World, pos: BlockPos, player: EntityPlayer, hand: EnumHand, facing: EnumFacing) {
-        player.sendMessage(TextComponentString("Heat: ${heatStorageComponent.heat}/${heatStorageComponent.heatCapacity} at " +
-                "${heatStorageComponent.drainEfficiency * 100}% (${(heatStorageComponent.heat * heatStorageComponent
-                        .drainEfficiency).toInt()})"))
+        player.sendMessage(TextComponentString("Matrix Coefficient: $matrixCoefficient"))
+    }
+
+    override fun writeToNBT(data: NBTTagCompound): NBTTagCompound {
+        super.writeToNBT(data)
+        data.setFloat(NBT_KEY_MATRIX_COEFFICIENT, matrixCoefficient)
+        return data
+    }
+
+    override fun readFromNBT(data: NBTTagCompound) {
+        super.readFromNBT(data)
+        this.matrixCoefficient = data.getFloat(NBT_KEY_MATRIX_COEFFICIENT)
     }
 }
