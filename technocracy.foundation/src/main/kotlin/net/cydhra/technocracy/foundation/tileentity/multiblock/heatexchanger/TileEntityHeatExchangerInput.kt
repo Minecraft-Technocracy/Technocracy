182c6eb2900447cb226cce7845ca4c24357ae87d
Cydhra <ubezl@student.kit.edu>
2019-07-07 22:18:46 +0200

    added fluid storage to heat exchanger ports

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/heatexchanger/TileEntityHeatExchangerInput.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/heatexchanger/TileEntityHeatExchangerInput.kt
@@ -1,17 +1,38 @@
 package net.cydhra.technocracy.foundation.tileentity.multiblock.heatexchanger
 
+import net.cydhra.technocracy.foundation.capabilities.fluid.DynamicFluidHandler
 import net.cydhra.technocracy.foundation.multiblock.HeatExchangerMultiBlock
+import net.cydhra.technocracy.foundation.tileentity.AggregatableDelegate
+import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatable
+import net.cydhra.technocracy.foundation.tileentity.components.FluidComponent
 import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityMultiBlockPart
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.fluids.FluidRegistry
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler
 
+@Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class TileEntityHeatExchangerInput : TileEntityMultiBlockPart<HeatExchangerMultiBlock>(HeatExchangerMultiBlock::class,
-        ::HeatExchangerMultiBlock) {
+        ::HeatExchangerMultiBlock), TCAggregatable by AggregatableDelegate() {
+
+    /**
+     * The fluid storage for internal usage
+     */
+    private val internalFluidHandler = DynamicFluidHandler(4000, allowedFluid = mutableListOf(),
+            tanktype = DynamicFluidHandler.TankType.INPUT)
+
+    private val fluidComponent = FluidComponent(internalFluidHandler, mutableSetOf(EnumFacing.NORTH, EnumFacing.EAST,
+            EnumFacing.SOUTH, EnumFacing.WEST))
+
+    init {
+        this.registerComponent(fluidComponent, "agent")
+    }
+
     override fun onMachineActivated() {}
 
     override fun onMachineDeactivated() {}
@@ -21,7 +42,16 @@ class TileEntityHeatExchangerInput : TileEntityMultiBlockPart<HeatExchangerMulti
     }
 
     override fun <T> getCapability(capability: Capability<T>, facing: EnumFacing?): T? {
-        return null
+        return this.castCapability(capability, facing) ?: super.getCapability(capability, facing)
+    }
+
+    override fun writeToNBT(data: NBTTagCompound): NBTTagCompound {
+        return this.serializeNBT(super.writeToNBT(data))
+    }
+
+    override fun readFromNBT(data: NBTTagCompound) {
+        super.readFromNBT(data)
+        this.deserializeNBT(data)
     }
 
     override fun onActivate(world: World, pos: BlockPos, player: EntityPlayer, hand: EnumHand, facing: EnumFacing) {
--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/heatexchanger/TileEntityHeatExchangerOutput.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/heatexchanger/TileEntityHeatExchangerOutput.kt
@@ -1,8 +1,13 @@
 package net.cydhra.technocracy.foundation.tileentity.multiblock.heatexchanger
 
+import net.cydhra.technocracy.foundation.capabilities.fluid.DynamicFluidHandler
 import net.cydhra.technocracy.foundation.multiblock.HeatExchangerMultiBlock
+import net.cydhra.technocracy.foundation.tileentity.AggregatableDelegate
+import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatable
+import net.cydhra.technocracy.foundation.tileentity.components.FluidComponent
 import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityMultiBlockPart
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
@@ -10,8 +15,20 @@ import net.minecraft.world.World
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler
 
+@Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class TileEntityHeatExchangerOutput : TileEntityMultiBlockPart<HeatExchangerMultiBlock>(HeatExchangerMultiBlock::class,
-        ::HeatExchangerMultiBlock) {
+        ::HeatExchangerMultiBlock), TCAggregatable by AggregatableDelegate() {
+
+    private val internalFluidHandler = DynamicFluidHandler(4000, allowedFluid = mutableListOf(),
+            tanktype = DynamicFluidHandler.TankType.OUTPUT)
+
+    private val fluidComponent = FluidComponent(internalFluidHandler, mutableSetOf(EnumFacing.NORTH, EnumFacing.EAST,
+            EnumFacing.SOUTH, EnumFacing.WEST))
+
+    init {
+        this.registerComponent(fluidComponent, "output")
+    }
+
     override fun onMachineActivated() {}
 
     override fun onMachineDeactivated() {}
@@ -21,7 +38,16 @@ class TileEntityHeatExchangerOutput : TileEntityMultiBlockPart<HeatExchangerMult
     }
 
     override fun <T> getCapability(capability: Capability<T>, facing: EnumFacing?): T? {
-        return null
+        return this.castCapability(capability, facing) ?: super.getCapability(capability, facing)
+    }
+
+    override fun writeToNBT(data: NBTTagCompound): NBTTagCompound {
+        return this.serializeNBT(super.writeToNBT(data))
+    }
+
+    override fun readFromNBT(data: NBTTagCompound) {
+        super.readFromNBT(data)
+        this.deserializeNBT(data)
     }
 
     override fun onActivate(world: World, pos: BlockPos, player: EntityPlayer, hand: EnumHand, facing: EnumFacing) {
