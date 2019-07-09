2e7873ae8c12bb97dbb64da84d01fad8513d73f4
Cydhra <ubezl@student.kit.edu>
2019-07-09 21:26:08 +0200

    refinery input exposes capabilities and stores components to NBT

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryInput.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryInput.kt
@@ -9,7 +9,10 @@ import net.cydhra.technocracy.foundation.tileentity.AggregatableDelegate
 import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatable
 import net.cydhra.technocracy.foundation.tileentity.components.FluidComponent
 import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityMultiBlockPart
+import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumFacing
+import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler
 
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class TileEntityRefineryInput : TileEntityMultiBlockPart<RefineryMultiBlock>(RefineryMultiBlock::class,
@@ -25,5 +28,22 @@ class TileEntityRefineryInput : TileEntityMultiBlockPart<RefineryMultiBlock>(Ref
 
     init {
         registerComponent(fluidComponent, "input")
+    }
+
+    override fun hasCapability(capability: Capability<*>, facing: EnumFacing?): Boolean {
+        return this.supportsCapability(capability, facing) || super.hasCapability(capability, facing)
+    }
+
+    override fun <T : Any?> getCapability(capability: Capability<T>, facing: EnumFacing?): T? {
+        return this.castCapability(capability, facing) ?: super.getCapability(capability, facing)
+    }
+
+    override fun writeToNBT(data: NBTTagCompound): NBTTagCompound {
+        return this.serializeNBT(super.writeToNBT(data))
+    }
+
+    override fun readFromNBT(data: NBTTagCompound) {
+        super.readFromNBT(data)
+        this.deserializeNBT(data)
     }
 }
