86b1867240e1bd4c6a72e542cf81466657833153
Cydhra <ubezl@student.kit.edu>
2019-07-09 21:32:27 +0200

    added energy component to refinery heater

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryHeater.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryHeater.kt
@@ -3,10 +3,36 @@ package net.cydhra.technocracy.foundation.tileentity.multiblock.refinery
 import net.cydhra.technocracy.foundation.multiblock.RefineryMultiBlock
 import net.cydhra.technocracy.foundation.tileentity.AggregatableDelegate
 import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatable
+import net.cydhra.technocracy.foundation.tileentity.components.EnergyStorageComponent
 import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityMultiBlockPart
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.util.EnumFacing
+import net.minecraftforge.common.capabilities.Capability
 
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class TileEntityRefineryHeater : TileEntityMultiBlockPart<RefineryMultiBlock>(RefineryMultiBlock::class,
         ::RefineryMultiBlock), TCAggregatable by AggregatableDelegate() {
 
+    private val energyStorageComponent: EnergyStorageComponent = EnergyStorageComponent(mutableSetOf(EnumFacing.DOWN))
+
+    init {
+        this.registerComponent(energyStorageComponent, "energy")
+    }
+
+    override fun writeToNBT(data: NBTTagCompound): NBTTagCompound {
+        return this.serializeNBT(super.writeToNBT(data))
+    }
+
+    override fun readFromNBT(data: NBTTagCompound) {
+        super.readFromNBT(data)
+        this.deserializeNBT(data)
+    }
+
+    override fun hasCapability(capability: Capability<*>, facing: EnumFacing?): Boolean {
+        return this.supportsCapability(capability, facing) || super.hasCapability(capability, facing)
+    }
+
+    override fun <T : Any?> getCapability(capability: Capability<T>, facing: EnumFacing?): T? {
+        return this.castCapability(capability, facing) ?: super.getCapability(capability, facing)
+    }
 }
