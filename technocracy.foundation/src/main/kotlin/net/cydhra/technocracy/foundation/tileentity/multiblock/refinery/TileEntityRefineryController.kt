64b8506b5cfc5abe2b4e53513c7d2d96df00f081
Cydhra <ubezl@student.kit.edu>
2019-07-09 23:40:59 +0200

    refinery controller also saves input fluid instead of ingress port

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryController.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryController.kt
@@ -1,6 +1,9 @@
 package net.cydhra.technocracy.foundation.tileentity.multiblock.refinery
 
 import net.cydhra.technocracy.foundation.capabilities.fluid.DynamicFluidHandler
+import net.cydhra.technocracy.foundation.liquids.general.heavyOilFluid
+import net.cydhra.technocracy.foundation.liquids.general.lightOilFluid
+import net.cydhra.technocracy.foundation.liquids.general.mineralOilFluid
 import net.cydhra.technocracy.foundation.multiblock.RefineryMultiBlock
 import net.cydhra.technocracy.foundation.tileentity.AggregatableDelegate
 import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatable
@@ -18,11 +21,18 @@ class TileEntityRefineryController : TileEntityMultiBlockPart<RefineryMultiBlock
     val bottomTank = DynamicFluidHandler(4000, allowedFluid = mutableListOf(),
             tanktype = DynamicFluidHandler.TankType.OUTPUT)
 
+    private val internalFluidHandler = DynamicFluidHandler(4000,
+            allowedFluid = mutableListOf(mineralOilFluid.hotFluid.name, heavyOilFluid.hotFluid.name,
+                    lightOilFluid.hotFluid.name),
+            tanktype = DynamicFluidHandler.TankType.INPUT)
 
+    val inputComponent = FluidComponent(internalFluidHandler, mutableSetOf(EnumFacing.NORTH, EnumFacing.EAST,
+            EnumFacing.SOUTH, EnumFacing.WEST))
     private val topOutput = FluidComponent(topTank, facing = mutableSetOf(*EnumFacing.values()))
     private val bottomOutput = FluidComponent(bottomTank, facing = mutableSetOf(*EnumFacing.values()))
 
     init {
+        this.registerComponent(inputComponent, "input")
         this.registerComponent(topOutput, "top")
         this.registerComponent(bottomOutput, "bottom")
     }
--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryInput.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryInput.kt
@@ -1,49 +1,24 @@
 package net.cydhra.technocracy.foundation.tileentity.multiblock.refinery
 
-import net.cydhra.technocracy.foundation.capabilities.fluid.DynamicFluidHandler
-import net.cydhra.technocracy.foundation.liquids.general.heavyOilFluid
-import net.cydhra.technocracy.foundation.liquids.general.lightOilFluid
-import net.cydhra.technocracy.foundation.liquids.general.mineralOilFluid
 import net.cydhra.technocracy.foundation.multiblock.RefineryMultiBlock
-import net.cydhra.technocracy.foundation.tileentity.AggregatableDelegate
-import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatable
-import net.cydhra.technocracy.foundation.tileentity.components.FluidComponent
 import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityMultiBlockPart
-import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumFacing
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler
 
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class TileEntityRefineryInput : TileEntityMultiBlockPart<RefineryMultiBlock>(RefineryMultiBlock::class,
-        ::RefineryMultiBlock), TCAggregatable by AggregatableDelegate() {
-
-    private val internalFluidHandler = DynamicFluidHandler(4000,
-            allowedFluid = mutableListOf(mineralOilFluid.hotFluid.name, heavyOilFluid.hotFluid.name,
-                    lightOilFluid.hotFluid.name),
-            tanktype = DynamicFluidHandler.TankType.INPUT)
-
-    val fluidComponent = FluidComponent(internalFluidHandler, mutableSetOf(EnumFacing.NORTH, EnumFacing.EAST,
-            EnumFacing.SOUTH, EnumFacing.WEST))
-
-    init {
-        registerComponent(fluidComponent, "input")
-    }
+        ::RefineryMultiBlock) {
 
     override fun hasCapability(capability: Capability<*>, facing: EnumFacing?): Boolean {
-        return this.supportsCapability(capability, facing) || super.hasCapability(capability, facing)
+        return capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY
     }
 
-    override fun <T : Any?> getCapability(capability: Capability<T>, facing: EnumFacing?): T? {
-        return this.castCapability(capability, facing) ?: super.getCapability(capability, facing)
-    }
-
-    override fun writeToNBT(data: NBTTagCompound): NBTTagCompound {
-        return this.serializeNBT(super.writeToNBT(data))
-    }
-
-    override fun readFromNBT(data: NBTTagCompound) {
-        super.readFromNBT(data)
-        this.deserializeNBT(data)
+    override fun <T> getCapability(capability: Capability<T>, facing: EnumFacing?): T? {
+        return if (hasCapability(capability, facing))
+            CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY
+                    .cast(this.multiblockController?.controllerTileEntity?.inputComponent?.fluid)
+        else
+            null
     }
 }
