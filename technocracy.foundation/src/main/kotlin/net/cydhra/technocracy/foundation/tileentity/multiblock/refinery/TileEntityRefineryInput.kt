88b17cd33f7c793f0dab6e99900dc3abb2ef3f7a
Cydhra <ubezl@student.kit.edu>
2019-07-09 20:26:47 +0200

    added a fluid tank to refinery input

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryInput.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/multiblock/refinery/TileEntityRefineryInput.kt
@@ -1,12 +1,24 @@
 package net.cydhra.technocracy.foundation.tileentity.multiblock.refinery
 
+import net.cydhra.technocracy.foundation.capabilities.fluid.DynamicFluidHandler
 import net.cydhra.technocracy.foundation.multiblock.RefineryMultiBlock
 import net.cydhra.technocracy.foundation.tileentity.AggregatableDelegate
 import net.cydhra.technocracy.foundation.tileentity.api.TCAggregatable
+import net.cydhra.technocracy.foundation.tileentity.components.FluidComponent
 import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityMultiBlockPart
+import net.minecraft.util.EnumFacing
 
 @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
 class TileEntityRefineryInput : TileEntityMultiBlockPart<RefineryMultiBlock>(RefineryMultiBlock::class,
         ::RefineryMultiBlock), TCAggregatable by AggregatableDelegate() {
 
+    private val internalFluidHandler = DynamicFluidHandler(4000, allowedFluid = mutableListOf(),
+            tanktype = DynamicFluidHandler.TankType.INPUT)
+
+    val fluidComponent = FluidComponent(internalFluidHandler, mutableSetOf(EnumFacing.NORTH, EnumFacing.EAST,
+            EnumFacing.SOUTH, EnumFacing.WEST))
+
+    init {
+        registerComponent(fluidComponent, "input")
+    }
 }
