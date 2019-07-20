fbb8a81dcbccc512075099ea9ff013f50bac1739
finn <fnkarstens@web.de>
2019-07-20 19:07:20 +0200

    fixed fucked up merge

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/waila/TCWailaPlugin.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/waila/TCWailaPlugin.kt
@@ -4,16 +4,23 @@ import mcp.mobius.waila.api.IWailaPlugin
 import mcp.mobius.waila.api.IWailaRegistrar
 import mcp.mobius.waila.api.WailaPlugin
 import net.cydhra.technocracy.foundation.blocks.MachineBlock
+import net.cydhra.technocracy.foundation.blocks.MultiBlockRotatableActiveBlock
 import net.cydhra.technocracy.foundation.integration.waila.providers.MachineProvider
+import net.cydhra.technocracy.foundation.integration.waila.providers.MultiBlockProvider
+import net.cydhra.technocracy.foundation.multiblock.BaseMultiBlock
 
 @WailaPlugin
 class TCWailaPlugin : IWailaPlugin {
 
-    val machineProvider  = MachineProvider()
+    val machineProvider = MachineProvider()
+    val multiBlockProvider = MultiBlockProvider()
 
     override fun register(registrar: IWailaRegistrar?) {
         registrar!!
         registrar.registerBodyProvider(machineProvider, MachineBlock::class.java)
         registrar.registerNBTProvider(machineProvider, MachineBlock::class.java)
+
+        registrar.registerBodyProvider(multiBlockProvider, MultiBlockRotatableActiveBlock::class.java)
+        registrar.registerNBTProvider(multiBlockProvider, MultiBlockRotatableActiveBlock::class.java)
     }
 }
