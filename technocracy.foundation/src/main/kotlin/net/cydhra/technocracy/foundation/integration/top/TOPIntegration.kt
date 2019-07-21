fc610885ff71b4c095e850ecd3c217904d8160d6
finn <fnkarstens@web.de>
2019-07-21 15:26:06 +0200

    added top integration for multiblocks

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/top/TOPIntegration.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/top/TOPIntegration.kt
@@ -2,12 +2,14 @@ package net.cydhra.technocracy.foundation.integration.top
 
 import mcjty.theoneprobe.TheOneProbe
 import net.cydhra.technocracy.foundation.integration.top.providers.MachineTOPProvider
+import net.cydhra.technocracy.foundation.integration.top.providers.MultiBlockTOPProvider
 
 
 class TOPIntegration {
 
     fun init() {
         TheOneProbe.theOneProbeImp.registerProvider(MachineTOPProvider())
+        TheOneProbe.theOneProbeImp.registerProvider(MultiBlockTOPProvider())
     }
 
 }
--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/top/providers/MultiBlockTOPProvider.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/top/providers/MultiBlockTOPProvider.kt
@@ -5,22 +5,23 @@ import mcjty.theoneprobe.api.IProbeInfo
 import mcjty.theoneprobe.api.IProbeInfoProvider
 import mcjty.theoneprobe.api.ProbeMode
 import mcjty.theoneprobe.apiimpl.styles.ProgressStyle
-import net.cydhra.technocracy.foundation.blocks.MachineBlock
-import net.cydhra.technocracy.foundation.tileentity.MachineTileEntity
+import net.cydhra.technocracy.foundation.blocks.MultiBlockRotatableActiveBlock
+import net.cydhra.technocracy.foundation.multiblock.BaseMultiBlock
 import net.cydhra.technocracy.foundation.tileentity.components.EnergyStorageComponent
+import net.cydhra.technocracy.foundation.tileentity.multiblock.TileEntityMultiBlockPart
 import net.minecraft.block.state.IBlockState
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.world.World
 
-class MachineTOPProvider : IProbeInfoProvider {
+class MultiBlockTOPProvider : IProbeInfoProvider {
 
     val energyStyle: ProgressStyle = ProgressStyle().suffix("RF").filledColor(0xffff0000.toInt()).borderColor(0xff555555.toInt()).alternateFilledColor(0xff000000.toInt())
 
     override fun addProbeInfo(mode: ProbeMode, probeInfo: IProbeInfo, player: EntityPlayer, world: World, blockState: IBlockState, data: IProbeHitData) {
-        if (blockState.block !is MachineBlock) return
-        val te = world.getTileEntity(data.pos) as? MachineTileEntity ?: return
+        if (blockState.block !is MultiBlockRotatableActiveBlock<*>) return
+        val te = world.getTileEntity(data.pos) as? TileEntityMultiBlockPart<*> ?: return
         val info = probeInfo.vertical()
-        te.getComponents().forEach {
+        (te.multiblockController as BaseMultiBlock).getComponents().forEach {
             if (it.second is EnergyStorageComponent) {
                 val component = it.second as EnergyStorageComponent
                 info.horizontal().progress(component.energyStorage.currentEnergy, component.energyStorage.capacity, energyStyle)
@@ -28,6 +29,5 @@ class MachineTOPProvider : IProbeInfoProvider {
         }
     }
 
-    override fun getID(): String = "technocracy.foundation.topintegration.machineprovider"
-
+    override fun getID(): String = "technocracy.foundation.topintegration.multiblockprovider"
 }
