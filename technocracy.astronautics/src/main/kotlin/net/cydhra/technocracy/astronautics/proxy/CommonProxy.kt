2b4cc4af11a3be43c3c4c7461809d9903c0274f1
Poke <platin_one@yahoo.de>
2019-07-22 03:21:34 +0200

    Made TileEntityManager, now without breaking the master

--- technocracy.astronautics/src/main/kotlin/net/cydhra/technocracy/astronautics/proxy/CommonProxy.kt
+++ technocracy.astronautics/src/main/kotlin/net/cydhra/technocracy/astronautics/proxy/CommonProxy.kt
@@ -1,10 +1,7 @@
 package net.cydhra.technocracy.astronautics.proxy
 
 import net.cydhra.technocracy.astronautics.TCAstronautics
-import net.cydhra.technocracy.astronautics.blocks.general.reinforcedConcreteBlock
-import net.cydhra.technocracy.astronautics.blocks.general.scaffoldBlock
-import net.cydhra.technocracy.astronautics.blocks.general.wetConcreteBlock
-import net.cydhra.technocracy.astronautics.blocks.general.wetReinforcedConcreteBlock
+import net.cydhra.technocracy.astronautics.blocks.general.*
 import net.cydhra.technocracy.astronautics.capabilities.satellites.DefaultSatelliteOrbit
 import net.cydhra.technocracy.astronautics.capabilities.satellites.ISatelliteOrbit
 import net.cydhra.technocracy.astronautics.capabilities.satellites.SatelliteOrbitStorage
@@ -12,9 +9,11 @@ import net.cydhra.technocracy.astronautics.client.astronauticsCreativeTabs
 import net.cydhra.technocracy.astronautics.client.model.concreteSprayer.ConcreteSprayerItemModel
 import net.cydhra.technocracy.astronautics.items.general.concreteCanItem
 import net.cydhra.technocracy.astronautics.items.general.concreteSprayerItem
+import net.cydhra.technocracy.astronautics.tileentity.RocketControllerTileEntity
 import net.cydhra.technocracy.foundation.blocks.general.BlockManager
 import net.cydhra.technocracy.foundation.items.general.ItemManager
 import net.cydhra.technocracy.foundation.liquids.general.FluidManager
+import net.cydhra.technocracy.foundation.tileentity.management.TileEntityManager
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.common.capabilities.CapabilityManager
 
@@ -23,20 +22,26 @@ open class CommonProxy {
     protected val blockManager = BlockManager(TCAstronautics.MODID, astronauticsCreativeTabs)
     protected val fluidManager = FluidManager(blockManager)
     protected val itemManager = ItemManager(TCAstronautics.MODID, astronauticsCreativeTabs)
+    protected var tileEntityManager = TileEntityManager(TCAstronautics.MODID)
+
 
     open fun preInit() {
         MinecraftForge.EVENT_BUS.register(blockManager)
         MinecraftForge.EVENT_BUS.register(fluidManager)
         MinecraftForge.EVENT_BUS.register(itemManager)
+        MinecraftForge.EVENT_BUS.register(tileEntityManager)
         MinecraftForge.EVENT_BUS.register(CapabilityEventBusHandler())
 
         blockManager.prepareBlocksForRegistration(scaffoldBlock)
         blockManager.prepareBlocksForRegistration(reinforcedConcreteBlock)
         blockManager.prepareBlocksForRegistration(wetConcreteBlock)
         blockManager.prepareBlocksForRegistration(wetReinforcedConcreteBlock)
+        blockManager.prepareBlocksForRegistration(rocketControllerBlock)
 
         itemManager.prepareItemForRegistration(concreteSprayerItem, ConcreteSprayerItemModel())
         itemManager.prepareItemForRegistration(concreteCanItem)
+
+        tileEntityManager.prepareTileEntityForRegistration(RocketControllerTileEntity::class)
     }
 
     open fun init() {
