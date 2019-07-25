cf6a384794e4328a6aaa6b7abbae3a0997193a81
finn <fnkarstens@web.de>
2019-07-25 20:02:50 +0200

    waila tooltip shows up at any part of a multiblock

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/waila/TCWailaPlugin.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/waila/TCWailaPlugin.kt
@@ -5,6 +5,7 @@ import mcp.mobius.waila.api.IWailaRegistrar
 import mcp.mobius.waila.api.WailaPlugin
 import net.cydhra.technocracy.foundation.blocks.MachineBlock
 import net.cydhra.technocracy.foundation.blocks.MultiBlockRotatableActiveBlock
+import net.cydhra.technocracy.foundation.blocks.PlainMultiBlockPartBlock
 import net.cydhra.technocracy.foundation.integration.waila.providers.MachineWailaProvider
 
 @WailaPlugin
@@ -13,9 +14,14 @@ class TCWailaPlugin : IWailaPlugin {
     val machineProvider = MachineWailaProvider()
 
     override fun register(registrar: IWailaRegistrar) {
+        // machines
         registrar.registerBodyProvider(machineProvider, MachineBlock::class.java)
         registrar.registerNBTProvider(machineProvider, MachineBlock::class.java)
+
+        // multiblocks
         registrar.registerBodyProvider(machineProvider, MultiBlockRotatableActiveBlock::class.java)
         registrar.registerNBTProvider(machineProvider, MultiBlockRotatableActiveBlock::class.java)
+        registrar.registerBodyProvider(machineProvider, PlainMultiBlockPartBlock::class.java)
+        registrar.registerNBTProvider(machineProvider, PlainMultiBlockPartBlock::class.java)
     }
 }
