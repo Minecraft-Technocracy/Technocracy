927ea787df410ea9f50d74cacbf056679456432d
finn <fnkarstens@web.de>
2019-07-25 19:55:24 +0200

    removed top multiblock provider

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/top/TOPIntegration.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/integration/top/TOPIntegration.kt
@@ -2,7 +2,6 @@ package net.cydhra.technocracy.foundation.integration.top
 
 import mcjty.theoneprobe.TheOneProbe
 import net.cydhra.technocracy.foundation.integration.top.providers.MachineTOPProvider
-import net.cydhra.technocracy.foundation.integration.top.providers.MultiBlockTOPProvider
 
 
 class TOPIntegration {
