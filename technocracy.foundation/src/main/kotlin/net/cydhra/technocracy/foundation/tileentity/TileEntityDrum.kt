7ad184ab3236a4fdac9c8d07e8e293a9ae8f7706
Poke <platin_one@yahoo.de>
2019-08-05 01:55:31 +0200

    fixed drum loading

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/TileEntityDrum.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/TileEntityDrum.kt
@@ -3,14 +3,19 @@ package net.cydhra.technocracy.foundation.tileentity
 import net.cydhra.technocracy.foundation.blocks.DrumBlock
 import net.cydhra.technocracy.foundation.capabilities.fluid.DynamicFluidHandler
 import net.cydhra.technocracy.foundation.tileentity.components.FluidComponent
+import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumFacing
 
 
-class TileEntityDrum(val type: Int = -1) : AggregatableTileEntity() {
-
-    val fluidCapability = DynamicFluidHandler(DrumBlock.DrumType.values()[type].amount, mutableListOf())
+class TileEntityDrum() : AggregatableTileEntity() {
+    val fluidCapability = DynamicFluidHandler(0, mutableListOf())
 
     private val fluidComponent = FluidComponent(fluidCapability, EnumFacing.values().toMutableSet())
+
+    override fun onLoad() {
+        val state = world.getBlockState(getPos())
+        fluidCapability.capacity = state.block.getMetaFromState(state)
+    }
 
     init {
         this.registerComponent(fluidComponent, "fluid")
