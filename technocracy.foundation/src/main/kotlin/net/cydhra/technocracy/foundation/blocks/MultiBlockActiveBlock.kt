6ecb6c9846b50ffea5ad863aba0776ad40b1c97a
Poke <platin_one@yahoo.de>
2019-08-05 01:01:35 +0200

    TileEntitys now can drop with nbt data

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/MultiBlockActiveBlock.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/MultiBlockActiveBlock.kt
@@ -8,12 +8,14 @@ import net.cydhra.technocracy.foundation.tileentity.api.TCMultiBlockActiveTileEn
 import net.minecraft.block.material.Material
 import net.minecraft.block.state.IBlockState
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.BlockRenderLayer
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.text.TextComponentTranslation
+import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
@@ -41,6 +43,10 @@ class MultiBlockActiveBlock<out T>(name: String, tileEntityConstructor: () -> T,
         }
 
         return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ)
+    }
+
+    override fun getDropItem(state: IBlockState, world: IBlockAccess, pos: BlockPos): ItemStack {
+        return ItemStack(this)
     }
 
     @SideOnly(Side.CLIENT)
