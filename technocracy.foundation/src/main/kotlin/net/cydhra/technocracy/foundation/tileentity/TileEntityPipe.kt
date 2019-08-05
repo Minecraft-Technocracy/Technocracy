aea1b58ad67de5f5135afecaa797262625c109cb
Cydhra <ubezl@student.kit.edu>
2019-08-05 20:13:04 +0200

    removed two methods from old network code from TEPipe

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/TileEntityPipe.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/tileentity/TileEntityPipe.kt
@@ -10,7 +10,6 @@ import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.AxisAlignedBB
 import net.minecraft.util.math.Vec3d
-import java.util.*
 
 
 class TileEntityPipe(meta: Int = -1) : AggregatableTileEntity() {
@@ -73,16 +72,6 @@ class TileEntityPipe(meta: Int = -1) : AggregatableTileEntity() {
 
     fun getInstalledTypes(): List<PipeType> {
         return listOf(*pipeTypes.types.toTypedArray())
-    }
-
-    fun getNetworkId(): UUID {
-        return networkComponent.uuid!!
-    }
-
-    fun setNetworkId(uuid: UUID): TileEntityPipe {
-        networkComponent.uuid = uuid
-        markForUpdate()
-        return this
     }
 
     override fun onLoad() {
