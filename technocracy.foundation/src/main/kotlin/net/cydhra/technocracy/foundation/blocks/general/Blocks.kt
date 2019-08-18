54cc0febd6c4e0c554301c8d9bc6664610e69de1
KotlinIsCool <ichmagbagga@gmail.com>
2019-08-18 20:36:35 +0200

    add lead-oxide electrode

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/general/Blocks.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/blocks/general/Blocks.kt
@@ -78,6 +78,7 @@ val capacitorControllerBlock = MultiBlockRotatableActiveBlock("capacitor_control
 val capacitorWallBlock = PlainMultiBlockPartBlock("capacitor_wall", ::TileEntityMultiBlockPartCapacitor)
 val capacitorConnectorBlock = PlainMultiBlockPartBlock("capacitor_connector", ::TileEntityMultiBlockPartCapacitor)
 val capacitorElectrodeBlock = PlainBlock("capacitor_electrode", Material.IRON)
+val capacitorOxidizedElectrodeBlock = PlainBlock("capacitor_oxidized_electrode", Material.IRON)
 val capacitorEnergyPortBlock = MultiBlockRotatableActiveBlock("capacitor_energy_port", ::TileEntityCapacitorEnergyPort)
 
 val oilSandBlock = OilSandBlock()
