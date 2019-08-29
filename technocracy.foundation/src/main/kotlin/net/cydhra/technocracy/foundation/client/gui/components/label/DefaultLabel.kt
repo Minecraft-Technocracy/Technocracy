93bb39b63e8227d09b7ef9655cf7a1c21549aa76
finn <32391033+deftkHD@users.noreply.github.com>
2019-08-29 21:17:52 +0200

    added custom gui for capacitor

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/client/gui/components/label/DefaultLabel.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/client/gui/components/label/DefaultLabel.kt
@@ -2,10 +2,10 @@ package net.cydhra.technocracy.foundation.client.gui.components.label
 
 import net.minecraft.client.Minecraft
 
-class DefaultLabel(posX: Int, posY: Int, text: String, val color: Int = 0xffffff) :
+class DefaultLabel(posX: Int, posY: Int, text: String, val color: Int = 0xffffff, val shadow: Boolean = true) :
         Label(posX, posY, text, Minecraft.getMinecraft().fontRenderer) {
 
     override fun draw(mouseX: Int, mouseY: Int, partialTicks: Float) {
-        fontRenderer.drawString(text, posX, posY, color)
+        fontRenderer.drawString(text, posX.toFloat(), posY.toFloat(), color, shadow)
     }
 }
