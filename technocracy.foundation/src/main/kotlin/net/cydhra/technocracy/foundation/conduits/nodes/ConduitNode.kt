e27e2fd020e2f99975503bbdcb7fb7706495bbb1
Cydhra <ubezl@student.kit.edu>
2019-09-07 16:51:40 +0200

    deleted unused data models for edges and nodes

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/nodes/ConduitNode.kt
+++
@@ -1,9 +0,0 @@
-package net.cydhra.technocracy.foundation.conduits.nodes
-
-import net.cydhra.technocracy.foundation.pipes.types.PipeType
-import net.minecraft.util.math.BlockPos
-
-/**
- * A node within the conduit network.
- */
-internal class ConduitNode(val pos: BlockPos, val type: PipeType)
