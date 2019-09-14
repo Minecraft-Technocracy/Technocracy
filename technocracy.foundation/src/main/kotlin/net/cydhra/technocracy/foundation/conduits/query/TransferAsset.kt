218b5c551e9a03e3ba38389d12ee83adca287987
Cydhra <ubezl@student.kit.edu>
2019-09-14 21:32:25 +0200

    No information about quantities makes sense for transfer assets as it is just a filter predicate for the network. A source may provide much more of an asset than a sink can actually accept.

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/TransferAsset.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/TransferAsset.kt
@@ -4,11 +4,9 @@ import net.cydhra.technocracy.foundation.conduits.types.PipeType
 
 /**
  * Any form of good that can be transferred using the conduit network. The asset is described using the pipe type
- * that transfers it as a content classification, a unique id to describe the actual item and an amount that is
- * transferred.
+ * that transfers it as a content classification and a unique id to describe the actual item.
  *
  * @param type pipe type that transfers this asset
  * @param content unique (within [type]) id for the content.
- * @param quantity how much of the asset this transfer describes
  */
-class TransferAsset(val type: PipeType, val content: Int, val quantity: Int)
+class TransferAsset(val type: PipeType, val content: Int)
