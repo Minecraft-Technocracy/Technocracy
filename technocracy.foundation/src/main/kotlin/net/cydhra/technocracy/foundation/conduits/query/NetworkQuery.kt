648ce9cc39a5be625d532985e5f997b211370374
Cydhra <ubezl@student.kit.edu>
2019-09-14 21:52:02 +0200

    updated doc of NetworkQuery

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/NetworkQuery.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/NetworkQuery.kt
@@ -9,6 +9,10 @@ import net.minecraft.util.math.ChunkPos
  * chunk that is affected by the query has been changed. If a chunk is updated, its validation counter increases,
  * indicating to cached queries, that it has been changed. Chunk unloading is checked against the
  * [net.cydhra.technocracy.foundation.conduits.ConduitNetwork].
+ * A single [NetworkQuery] instance may contain multiple routes for different [TransferAsset]s, as a found route may
+ * be cached while transferring a different asset and results about unavailable routes may be cached as well.
+ * Depending on the routing strategy, even more available routes may be cached here. If any of the routes becomes
+ * invalid, all cached routes are marked as invalid, though.
  * TODO: find a way to invalidate a query if a chunk containing reachable network parts is loaded, that is, a chunk
  *  next to an already watched chunk is suddenly available and offers sinks that might be preferable within the
  *  current strategy.
@@ -18,14 +22,18 @@ open class NetworkQuery(val providerSink: TransitSink) {
     /**
      * A list of chunks affected by this query. If any of them updates, this query must be invalid.
      */
-    protected val affectedChunks: MutableSet<ChunkPos> = mutableSetOf()
+    private val affectedChunks: MutableSet<ChunkPos> = mutableSetOf()
 
     /**
      * A list of chunks that are technically reachable from this network but are currently not loaded. If any of them
      * is loaded, the query must be invalid.
      */
-    protected val reachableUnavailableChunks: MutableSet<ChunkPos> = mutableSetOf()
+    private val reachableUnavailableChunks: MutableSet<ChunkPos> = mutableSetOf()
 
+    /**
+     * Check wether all cached routes are still valid. If any of them becomes invalid, false is returned and this
+     * instance may not be used again.
+     */
     fun isValid(): Boolean {
         TODO()
     }
