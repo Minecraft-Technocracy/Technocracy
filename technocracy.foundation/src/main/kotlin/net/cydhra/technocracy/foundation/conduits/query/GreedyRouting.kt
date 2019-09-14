84c78a6c380ffd3e8fb47779e4f9658171b6a5fe
Cydhra <ubezl@student.kit.edu>
2019-09-14 21:45:34 +0200

    routing shall be done in canTransfer() and actual transfer in transferAsset()

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/GreedyRouting.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/GreedyRouting.kt
@@ -2,16 +2,23 @@ package net.cydhra.technocracy.foundation.conduits.query
 
 import net.cydhra.technocracy.foundation.conduits.transit.TransitSink
 
+/**
+ * The simplest routing strategy. It will always transfer items to the closest available sink.
+ */
 object GreedyRouting : RoutingStrategy<GreedyQuery> {
-    override fun query(providerSink: TransitSink): GreedyQuery {
+    override fun prepareQuery(providerSink: TransitSink): GreedyQuery {
+        return GreedyQuery(providerSink)
+    }
+
+    override fun canTransfer(query: GreedyQuery, transferAsset: TransferAsset): Boolean {
         TODO("not implemented")
     }
 
-    override fun provideTargetSink(route: GreedyQuery): TransitSink {
+    override fun transferAsset(query: GreedyQuery, transferAsset: TransferAsset) {
         TODO("not implemented")
     }
 }
 
 class GreedyQuery(providerSink: TransitSink) : NetworkQuery(providerSink) {
-
+    val routes = HashMap<TransferAsset, TransitSink>()
 }
--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/RoundRobinRouting.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/RoundRobinRouting.kt
@@ -2,15 +2,19 @@ package net.cydhra.technocracy.foundation.conduits.query
 
 import net.cydhra.technocracy.foundation.conduits.transit.TransitSink
 
+
 object RoundRobinRouting : RoutingStrategy<RRQuery> {
-    override fun query(providerSink: TransitSink): RRQuery {
+    override fun prepareQuery(providerSink: TransitSink): RRQuery {
         TODO("not implemented")
     }
 
-    override fun provideTargetSink(route: RRQuery): TransitSink {
+    override fun canTransfer(query: RRQuery, transferAsset: TransferAsset): Boolean {
         TODO("not implemented")
     }
 
+    override fun transferAsset(query: RRQuery, transferAsset: TransferAsset) {
+        TODO("not implemented")
+    }
 }
 
 class RRQuery(providerSink: TransitSink) : NetworkQuery(providerSink)
