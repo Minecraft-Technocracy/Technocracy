56dd4a06da4dba5c5a3a55b7853a798bfe854c32
Cydhra <ubezl@student.kit.edu>
2019-09-14 22:09:39 +0200

    implement a strategy pattern for TransferAssets that can perform a transfer

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/TransferAsset.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/query/TransferAsset.kt
@@ -1,12 +1,39 @@
 package net.cydhra.technocracy.foundation.conduits.query
 
+import net.cydhra.technocracy.foundation.conduits.transit.TransitSink
 import net.cydhra.technocracy.foundation.conduits.types.PipeType
 
 /**
  * Any form of good that can be transferred using the conduit network. The asset is described using the pipe type
- * that transfers it as a content classification and a unique id to describe the actual item.
+ * that transfers it as a content classification and a unique id to describe the actual item. This class also handles
+ * the actual transfer, as type-safety can be guaranteed this way and contents can be moved directly without cloning
+ * anything, which could potentially destroy meta data.
  *
  * @param type pipe type that transfers this asset
  * @param content unique (within [type]) id for the content.
  */
-class TransferAsset(val type: PipeType, val content: Int)
+abstract class TransferAsset(val type: PipeType, val content: Int) {
+
+    /**
+     * Actually perform a transfer safely. This is a strategy pattern to transfer goods between two compatible sinks.
+     */
+    abstract fun performTransfer(providerSink: TransitSink, target: TransitSink)
+}
+
+class ItemTransferAsset(content: Int) : TransferAsset(PipeType.ITEM, content) {
+    override fun performTransfer(providerSink: TransitSink, target: TransitSink) {
+        TODO("not implemented")
+    }
+}
+
+class FluidTransferAsset(content: Int) : TransferAsset(PipeType.FLUID, content) {
+    override fun performTransfer(providerSink: TransitSink, target: TransitSink) {
+        TODO("not implemented")
+    }
+}
+
+class EnergyTransferAsset(content: Int) : TransferAsset(PipeType.ENERGY, content) {
+    override fun performTransfer(providerSink: TransitSink, target: TransitSink) {
+        TODO("not implemented")
+    }
+}
