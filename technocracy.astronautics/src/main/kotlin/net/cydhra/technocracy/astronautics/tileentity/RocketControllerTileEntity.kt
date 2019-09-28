481507e5c203b1191adb5c0429cff9ae966f39bf
Poke <platin_one@yahoo.de>
2019-09-28 15:28:06 +0200

    changed rocket tank texture

--- technocracy.astronautics/src/main/kotlin/net/cydhra/technocracy/astronautics/tileentity/RocketControllerTileEntity.kt
+++
@@ -1,6 +0,0 @@
-package net.cydhra.technocracy.astronautics.tileentity
-
-import net.cydhra.technocracy.foundation.tileentity.AggregatableTileEntity
-
-class RocketControllerTileEntity : AggregatableTileEntity() {
-}
