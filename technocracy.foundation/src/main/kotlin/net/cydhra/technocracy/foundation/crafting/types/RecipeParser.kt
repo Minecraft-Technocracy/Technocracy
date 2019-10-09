eaf81dffe6eecb8298ba034f3e9a8c0b61b09cb8
Cydhra <ubezl@student.kit.edu>
2019-10-09 14:20:52 +0200

    documentation for RecipeParser interface

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/crafting/types/RecipeParser.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/crafting/types/RecipeParser.kt
@@ -4,7 +4,16 @@ import com.google.gson.JsonObject
 import net.cydhra.technocracy.foundation.crafting.IMachineRecipe
 import net.minecraftforge.common.crafting.JsonContext
 
+/**
+ * Interface describing a parser for custum recipe types of machines of this mod
+ */
 interface RecipeParser<T : IMachineRecipe> {
 
+    /**
+     * Parse a recipe of type [T] from given json object
+     *
+     * @param json the json object as it is defined in the recipe resource asset
+     * @param context json parsing context
+     */
     fun process(json: JsonObject, context: JsonContext): T
 }
