38e09dec52fa6f060d15f14950ce125ce749d645
Cydhra <ubezl@student.kit.edu>
2019-12-13 18:53:36 +0100

    changed upgrade parameter definition

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/tileentities/upgrades/Upgrades.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/tileentities/upgrades/Upgrades.kt
@@ -6,7 +6,13 @@ import net.cydhra.technocracy.foundation.model.tileentities.api.upgrades.Machine
 const val MACHINE_UPGRADE_ENERGY: MachineUpgradeParameter = "energy"
 const val MACHINE_UPGRADE_SPEED: MachineUpgradeParameter = "speed"
 const val MACHINE_UPGRADE_ADDITIVE_CONSUMPTION: MachineUpgradeParameter = "additive"
-const val MACHINE_UPGRADE_ACTIVE_COOLING: MachineUpgradeParameter = "cooling"
+
+/**
+ * This parameter type is used for all upgrades that do something to the machine but do not rely on a specific
+ * machine parameter to be present. For example, [ActiveCoolingUpgrade] is a generic-parameter upgrade, as it can be
+ * used on all machines per se.
+ */
+const val MACHINE_UPGRADE_GENERIC: MachineUpgradeParameter = "generic"
 
 /**
  * A multiplier on machine energy consumption
@@ -24,6 +30,6 @@ class SpeedMultiplier(val multiplier: Double) : MachineUpgrade(MACHINE_UPGRADE_S
 class AdditiveConsumptionMultiplier(val multiplier: Double) : MachineUpgrade(MACHINE_UPGRADE_ADDITIVE_CONSUMPTION)
 
 /**
- * An upgrade that enables the machine to use coolants for speed increase
+ * An upgrade that enables the machine to use coolants for speed increase.
  */
-class ActiveCoolingUpgrade : MachineUpgrade(MACHINE_UPGRADE_ACTIVE_COOLING)
+class ActiveCoolingUpgrade : MachineUpgrade(MACHINE_UPGRADE_GENERIC)
