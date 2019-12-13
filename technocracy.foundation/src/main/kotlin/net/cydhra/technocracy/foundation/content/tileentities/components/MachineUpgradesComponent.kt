0cd2c9249ee24879631754fa327d4894f499b68e
Cydhra <ubezl@student.kit.edu>
2019-12-13 18:55:01 +0100

    javadoc and renamed

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/tileentities/components/MachineUpgradesComponent.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/tileentities/components/MachineUpgradesComponent.kt
@@ -2,12 +2,18 @@ package net.cydhra.technocracy.foundation.content.tileentities.components
 
 import net.cydhra.technocracy.foundation.model.tileentities.api.components.AbstractComponent
 import net.cydhra.technocracy.foundation.model.tileentities.api.components.ComponentType
+import net.cydhra.technocracy.foundation.model.tileentities.api.upgrades.MachineUpgradeParameter
 import net.minecraft.nbt.NBTTagCompound
 
 /**
  * A machine component that handles all machine upgrades.
+ *
+ * @param supportedUpgradeTypes a set of supported upgrade types. If a player tries to install an upgrade into the
+ * machine, all of its upgrade types must be supported for installation to work.
+ * @param numberOfUpgradeSlots how many upgrade slots the machine has.
  */
-class MachineUpgradesComponents : AbstractComponent() {
+class MachineUpgradesComponent(vararg val supportedUpgradeTypes: MachineUpgradeParameter,
+                               val numberOfUpgradeSlots: Int) : AbstractComponent() {
 
     companion object {
         private const val NBT_KEY_UPGRADES = "upgrades"
