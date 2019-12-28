63a048f2a92cfee71500aac684247f2413e03893
Cydhra <ubezl@student.kit.edu>
2019-12-28 01:30:50 +0100

    added a ton of speed upgrades and some energy upgrades

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/items/Items.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/items/Items.kt
@@ -69,6 +69,11 @@ val mechSpeedUp2Item = UpgradeItem("mech_speed_up_2", MachineUpgradeClass.MECHAN
 val mechSpeedUp3Item = UpgradeItem("mech_speed_up_3", MachineUpgradeClass.MECHANICAL,
         SpeedMultiplier(1.0), EnergyMultiplier(1.5))
 
+val mechEnergyUp1Item = UpgradeItem("mech_energy_up_1", MachineUpgradeClass.MECHANICAL,
+        SpeedMultiplier(-0.4), EnergyMultiplier(-0.35))
+val mechEnergyUp2Item = UpgradeItem("mech_energy_up_2", MachineUpgradeClass.MECHANICAL,
+        SpeedMultiplier(-0.7), EnergyMultiplier(-0.7))
+
 val chemSpeedUp1Item = UpgradeItem("chem_speed_up_1", MachineUpgradeClass.CHEMICAL,
         SpeedMultiplier(0.25), EnergyMultiplier(0.3))
 val chemSpeedUp2Item = UpgradeItem("chem_speed_up_2", MachineUpgradeClass.CHEMICAL,
@@ -77,6 +82,53 @@ val chemSpeedUp3Item = UpgradeItem("chem_speed_up_3", MachineUpgradeClass.CHEMIC
         SpeedMultiplier(1.0), EnergyMultiplier(1.5))
 val chemSpeedUp4Item = UpgradeItem("chem_speed_up_4", MachineUpgradeClass.CHEMICAL,
         SpeedMultiplier(-0.5), EnergyMultiplier(-0.3), AdditiveConsumptionMultiplier(-0.3))
+
+val thermSpeedUp1Item = UpgradeItem("therm_speed_up_1", MachineUpgradeClass.THERMAL,
+        SpeedMultiplier(0.25), EnergyMultiplier(0.3))
+val thermSpeedUp2Item = UpgradeItem("therm_speed_up_2", MachineUpgradeClass.THERMAL,
+        SpeedMultiplier(0.5), EnergyMultiplier(0.8))
+val thermSpeedUp3Item = UpgradeItem("therm_speed_up_3", MachineUpgradeClass.THERMAL,
+        SpeedMultiplier(1.0), EnergyMultiplier(1.8))
+
+val thermEnergyUp1Item = UpgradeItem("therm_energy_up_1", MachineUpgradeClass.THERMAL,
+        SpeedMultiplier(-0.3), EnergyMultiplier(-0.4))
+val thermEnergyUp2Item = UpgradeItem("therm_energy_up_2", MachineUpgradeClass.THERMAL,
+        EnergyMultiplier(-0.8), AdditiveConsumptionMultiplier(0.8))
+
+val elecSpeedUp1Item = UpgradeItem("elec_speed_up_1", MachineUpgradeClass.ELECTRICAL,
+        SpeedMultiplier(0.25), EnergyMultiplier(0.3))
+val elecSpeedUp2Item = UpgradeItem("elec_speed_up_2", MachineUpgradeClass.ELECTRICAL,
+        SpeedMultiplier(0.5), EnergyMultiplier(0.75))
+val elecSpeedUp3Item = UpgradeItem("elec_speed_up_3", MachineUpgradeClass.ELECTRICAL,
+        SpeedMultiplier(1.0), EnergyMultiplier(1.6))
+
+val elecEnergyUp1Item = UpgradeItem("elec_energy_up_1", MachineUpgradeClass.ELECTRICAL,
+        SpeedMultiplier(-0.1), EnergyMultiplier(-0.2))
+val elecEnergyUp2Item = UpgradeItem("elec_energy_up_2", MachineUpgradeClass.ELECTRICAL,
+        SpeedMultiplier(-0.2), EnergyMultiplier(-0.5))
+
+val compSpeedUp1Item = UpgradeItem("comp_speed_up_1", MachineUpgradeClass.COMPUTER,
+        SpeedMultiplier(0.25), EnergyMultiplier(0.4))
+val compSpeedUp2Item = UpgradeItem("comp_speed_up_2", MachineUpgradeClass.COMPUTER,
+        SpeedMultiplier(0.5), EnergyMultiplier(0.9))
+val compSpeedUp3Item = UpgradeItem("comp_speed_up_3", MachineUpgradeClass.COMPUTER,
+        SpeedMultiplier(1.1), EnergyMultiplier(1.4))
+val compSpeedUp4Item = UpgradeItem("comp_speed_up_4", MachineUpgradeClass.COMPUTER,
+        SpeedMultiplier(1.9), EnergyMultiplier(2.5))
+
+val optSpeedUp1Item = UpgradeItem("opt_speed_up_1", MachineUpgradeClass.OPTICAL,
+        SpeedMultiplier(0.25), EnergyMultiplier(0.3))
+val optSpeedUp2Item = UpgradeItem("opt_speed_up_2", MachineUpgradeClass.OPTICAL,
+        SpeedMultiplier(0.5), EnergyMultiplier(0.65))
+val optSpeedUp3Item = UpgradeItem("opt_speed_up_3", MachineUpgradeClass.OPTICAL,
+        SpeedMultiplier(1.0), EnergyMultiplier(1.4))
+
+val nucSpeedUp1Item = UpgradeItem("nuc_speed_up_1", MachineUpgradeClass.NUCLEAR,
+        SpeedMultiplier(0.25), EnergyMultiplier(0.25))
+val nucSpeedUp2Item = UpgradeItem("nuc_speed_up_2", MachineUpgradeClass.NUCLEAR,
+        SpeedMultiplier(0.5), EnergyMultiplier(0.8))
+val nucSpeedUp3Item = UpgradeItem("nuc_speed_up_3", MachineUpgradeClass.NUCLEAR,
+        SpeedMultiplier(1.0), EnergyMultiplier(2.2))
 
 val mechLubricantUpItem = UpgradeItem("mech_lubricant_up", MachineUpgradeClass.MECHANICAL,
         SpeedMultiplier(2.0), LubricantUpgrade())
