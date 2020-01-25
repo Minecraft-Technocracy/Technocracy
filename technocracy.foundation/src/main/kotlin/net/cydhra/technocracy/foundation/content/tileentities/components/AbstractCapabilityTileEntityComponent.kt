56f58262a8ad5409eb0137cd972d876f7fa06d51
Cydhra <ubezl@student.kit.edu>
2020-01-25 12:26:36 +0100

    AbstractCapabilityTileEntityComponent acts like a capability provider, so it is one

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/tileentities/components/AbstractCapabilityTileEntityComponent.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/tileentities/components/AbstractCapabilityTileEntityComponent.kt
@@ -1,30 +1,11 @@
 package net.cydhra.technocracy.foundation.content.tileentities.components
 
 import net.cydhra.technocracy.foundation.model.tileentities.api.components.AbstractTileEntityComponent
-import net.minecraft.util.EnumFacing
-import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.common.capabilities.ICapabilityProvider
 
 /**
  * Defines structure for capabilities that also implement a capability.
  */
-abstract class AbstractCapabilityTileEntityComponent : AbstractTileEntityComponent() {
+abstract class AbstractCapabilityTileEntityComponent : AbstractTileEntityComponent(), ICapabilityProvider {
 
-    /**
-     * Returns true, if this component implements the capability that is given through [capability].
-     *
-     * @param capability the queried capability
-     * @param facing for side-depending capabilities the queried side is given
-     *
-     * @return true, if this component offers the queried capability on given block face
-     */
-    abstract fun hasCapability(capability: Capability<*>, facing: EnumFacing?): Boolean
-
-    /**
-     * Get the capability instance of the queried capability casted to [T]
-     *
-     * @param capability queried capability
-     * @param facing queried block face
-     * @param T runtime type of requested capability that is expected by caller
-     */
-    abstract fun <T> getCapability(capability: Capability<T>, facing: EnumFacing?): T?
 }
