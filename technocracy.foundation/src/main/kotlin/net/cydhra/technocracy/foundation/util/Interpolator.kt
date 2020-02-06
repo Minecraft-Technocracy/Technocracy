37d2517a19f65c76e53d65d7e033d12fe2116372
Poke <platin_one@yahoo.de>
2020-02-06 21:12:40 +0100

    added an interpolator

---
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/util/Interpolator.kt
@@ -0,0 +1,74 @@
+package net.cydhra.technocracy.foundation.util
+
+import org.lwjgl.util.vector.Vector3f
+import kotlin.math.abs
+
+/**
+ * Created by Flo on 06.02.2020
+ */
+object Interpolator {
+
+    fun linearInterpolate(from: Float, to: Float, timeFrom: Float, timeTo: Float, currentTime: Float): Float {
+        val delta = currentTime - timeFrom
+        val distance = timeTo - timeFrom
+        val percentage = delta / distance
+
+        return from + (to - from) * percentage
+    }
+
+    class InterpolationCycle<T : InterpolateableValue<T>> {
+        private val steps = mutableListOf<InterpolationStep<T>>()
+
+        fun addStep(value: T, time: Float): InterpolationCycle<T> {
+            steps.add(InterpolationStep(time, value))
+            return this
+        }
+
+        fun getInterpolated(time: Float): T {
+            val current = getCurrent(time)
+            if (current.time > time) return current.value
+            val next = getNext(time) ?: return current.value
+
+            return interpolate(current, next, time)
+        }
+
+        fun getCurrent(time: Float): InterpolationStep<T> {
+            return steps.filter { it.time < time }.maxBy { it.time } ?: steps.minBy { it.time } ?: steps.first()
+        }
+
+        fun getNext(time: Float): InterpolationStep<T>? {
+            val current = getCurrent(time)
+            return steps.filter { it != current }.filter { it.time > current.time }.minBy { abs(it.time - current.time) }
+        }
+
+        private fun interpolate(from: InterpolationStep<T>, to: InterpolationStep<T>, time: Float): T {
+            return from.value.interpolate(to.value, from.time, to.time, time)
+        }
+    }
+
+    class InterpolationStep<T : InterpolateableValue<T>>(val time: Float, val value: T)
+
+    abstract class InterpolateableValue<T> {
+        abstract fun interpolate(to: T, timeFrom: Float, timeTo: Float, currentTime: Float): T
+    }
+
+    class InterpolateFloat(val value: Float) : InterpolateableValue<InterpolateFloat>() {
+        override fun interpolate(to: InterpolateFloat, timeFrom: Float, timeTo: Float, currentTime: Float): InterpolateFloat {
+            return InterpolateFloat(linearInterpolate(value, to.value, timeFrom, timeTo, currentTime))
+        }
+    }
+
+    class PosLook(val pos: Vector3f, val look: Vector3f) : InterpolateableValue<PosLook>() {
+        override fun interpolate(to: PosLook, timeFrom: Float, timeTo: Float, currentTime: Float): PosLook {
+            val x = linearInterpolate(pos.x, to.pos.x, timeFrom, timeTo, currentTime)
+            val y = linearInterpolate(pos.y, to.pos.y, timeFrom, timeTo, currentTime)
+            val z = linearInterpolate(pos.z, to.pos.z, timeFrom, timeTo, currentTime)
+
+            val yaw = linearInterpolate(look.x, to.look.x, timeFrom, timeTo, currentTime)
+            val pitch = linearInterpolate(look.y, to.look.y, timeFrom, timeTo, currentTime)
+            val roll = linearInterpolate(look.z, to.look.z, timeFrom, timeTo, currentTime)
+
+            return PosLook(Vector3f(x, y, z), Vector3f(yaw, pitch, roll))
+        }
+    }
+}
