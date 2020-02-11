1ee0c909d6ad179022d440ebe33ae1971d94ce8c
Tim <ichmagbagga@gmail.com>
2020-02-11 20:31:36 +0100

    added basic structure for saline mb

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/multiblock/SalineMultiBlock.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/content/multiblock/SalineMultiBlock.kt
@@ -1,9 +1,12 @@
 package net.cydhra.technocracy.foundation.content.multiblock
 
+import it.zerono.mods.zerocore.api.multiblock.validation.IMultiblockValidator
 import net.cydhra.technocracy.foundation.content.blocks.*
+import net.cydhra.technocracy.foundation.content.tileentities.multiblock.saline.*
 import net.cydhra.technocracy.foundation.model.components.IComponent
 import net.cydhra.technocracy.foundation.model.multiblock.api.TiledBaseMultiBlock
 import net.minecraft.init.Blocks
+import net.minecraft.util.EnumFacing
 import net.minecraft.world.World
 import java.util.function.Predicate
 
@@ -32,8 +35,59 @@ class SalineMultiBlock(world: World) : TiledBaseMultiBlock(
         world = world
 ) {
 
+    var controllerTileEntity: TileEntitySalineController? = null
+
+    override fun isMachineWhole(validatorCallback: IMultiblockValidator): Boolean {
+        if (!super.isMachineWhole(validatorCallback)) return false
+
+        return assemble(validatorCallback) {
+            val controllers = mutableListOf<TileEntitySalineController>()
+            val fluidInputs = mutableListOf<TileEntitySalineFluidInput>()
+            val fluidOutputs = mutableListOf<TileEntitySalineFluidOutput>()
+            val heatingAgentInputs = mutableListOf<TileEntitySalineHeatingAgentInput>()
+            val heatingAgentOutputs = mutableListOf<TileEntitySalineHeatingAgentOutput>()
+
+            var edgeCount = 0
+            tiles.forEach {
+                //Top and right edge
+                edgeCount += 2
+                //Left edge
+                if(!it.adjacentTileSides.contains(EnumFacing.WEST))
+                    edgeCount++
+                //Bottom edge
+                if(!it.adjacentTileSides.contains(EnumFacing.SOUTH))
+                    edgeCount++
+            }
+
+            collect(salineControllerBlock.unlocalizedName, controllers, 1)
+            collect(salineFluidInputBlock.unlocalizedName, fluidInputs, edgeCount)
+            collect(salineFluidOutputBlock.unlocalizedName, fluidOutputs, tiles.size)
+            collect(salineHeatingAgentInputBlock.unlocalizedName, heatingAgentInputs, 0 until edgeCount)
+            collect(salineHeatingAgentOutputBlock.unlocalizedName, heatingAgentOutputs, 0 until edgeCount)
+
+            finishUp {
+                this@SalineMultiBlock.controllerTileEntity = controllers.first()
+
+                return@finishUp this@SalineMultiBlock.recalculatePhysics(validatorCallback, fluidInputs, fluidOutputs,
+                        heatingAgentInputs, heatingAgentOutputs)
+            }
+        }
+    }
+
+    private fun recalculatePhysics(validatorCallback: IMultiblockValidator,
+                                   fluidInputs: MutableList<TileEntitySalineFluidInput>,
+                                   fluidOutputs: MutableList<TileEntitySalineFluidOutput>,
+                                   heatingAgentInputs: MutableList<TileEntitySalineHeatingAgentInput>,
+                                   heatingAgentOutputs: MutableList<TileEntitySalineHeatingAgentOutput>): Boolean {
+
+        return true
+    }
+
     override fun getComponents(): MutableList<Pair<String, IComponent>> {
-        return mutableListOf()
+        val components = mutableListOf<Pair<String, IComponent>>()
+        if (controllerTileEntity != null)
+            components.addAll(controllerTileEntity!!.getComponents())
+        return components
     }
 
     override fun updateServer(): Boolean {
