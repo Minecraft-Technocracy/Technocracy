5b80d60959d033e9722f966a29cf1dd4b6763860
Cydhra <ubezl@student.kit.edu>
2020-02-29 01:32:23 +0100

    noted one-fluid-assertion

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/data/crafting/special/HeatRecipe.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/data/crafting/special/HeatRecipe.kt
@@ -7,6 +7,7 @@ import net.minecraftforge.fluids.Fluid
  * A recipe for heat exchanging fluids. It is defined by the temperature difference of both fluids and how much heat
  * per degree of temperature this fluid stores. Thus, cooling one bucket of this fluid from hot to cold will result in
  * ``temperatureDifference * heatPerDegree`` heat.
+ * It is always asserted, that no fluid is part of more than one recipe!
  */
 class HeatRecipe(
         val coldFluid: Fluid,
