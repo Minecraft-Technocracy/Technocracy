af97319c485d39374f90d78e9fbe88bad2ecc826
Poke <platin_one@yahoo.de>
2020-03-01 16:51:07 +0100

    added tileentity unlisted property

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/util/propertys/CustomPropertys.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/util/propertys/CustomPropertys.kt
@@ -1,5 +1,6 @@
 package net.cydhra.technocracy.foundation.util.propertys
 
+import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.math.BlockPos
 import net.minecraftforge.fluids.FluidStack
 import javax.vecmath.Vector3f
@@ -9,3 +10,4 @@ val POSITION = UnlistedProperty("position", BlockPos::class.java)
 val FLUIDSTACK = UnlistedProperty("fluid_stack", FluidStack::class.java)
 val DIMENSIONS = UnlistedProperty("dimensions", Vector3f::class.java)
 val TANKSIZE = UnlistedProperty("tanksize", Integer::class.java)
+val TILEENTITY = UnlistedProperty("tileentity", TileEntity::class.java)
