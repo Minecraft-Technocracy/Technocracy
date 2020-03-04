a6bf7b1dbf11e3ba8f25f20395f04d9e1aa5a020
Cydhra <ubezl@student.kit.edu>
2020-03-04 21:16:49 +0100

    reorganized upgrade gui tab

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/client/gui/machine/MachineUpgradesTab.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/client/gui/machine/MachineUpgradesTab.kt
@@ -3,10 +3,10 @@ package net.cydhra.technocracy.foundation.client.gui.machine
 import net.cydhra.technocracy.foundation.client.gui.TCGui
 import net.cydhra.technocracy.foundation.client.gui.TCIcon
 import net.cydhra.technocracy.foundation.client.gui.TCTab
+import net.cydhra.technocracy.foundation.client.gui.components.label.WrappingLabel
 import net.cydhra.technocracy.foundation.client.gui.components.slot.TCSlotIO
 import net.cydhra.technocracy.foundation.content.items.wrenchItem
 import net.cydhra.technocracy.foundation.content.tileentities.components.MachineUpgradesTileEntityComponent
-import net.minecraft.client.Minecraft
 import net.minecraft.entity.player.EntityPlayer
 
 class MachineUpgradesTab(parent: TCGui,
@@ -14,26 +14,61 @@ class MachineUpgradesTab(parent: TCGui,
                          private val player: EntityPlayer?) :
         TCTab("Upgrades", parent, icon = TCIcon(wrenchItem)) {
 
+    companion object {
+        const val PADDING_LEFT = 8
+        const val PADDING_TOP = 20
+        const val PADDING_RIGHT = PADDING_LEFT
+        const val SLOT_WIDTH_PLUS_PADDING = 18
+        const val UPGRADE_SLOTS_PER_ROW = 3
+        const val INFO_LABEL_OFFSET = PADDING_LEFT + UPGRADE_SLOTS_PER_ROW * SLOT_WIDTH_PLUS_PADDING + PADDING_LEFT
+    }
+
+    private val lableWidth = (parent.guiWidth - PADDING_RIGHT - INFO_LABEL_OFFSET) / 2
+    private val infoTitleLabel = WrappingLabel(
+            posX = INFO_LABEL_OFFSET,
+            posY = PADDING_TOP,
+            maxWidth = lableWidth,
+            scaling = 0.7,
+            text = ""
+    )
+
+    private val infoContentLabel = WrappingLabel(
+            posX = INFO_LABEL_OFFSET + lableWidth,
+            posY = PADDING_TOP,
+            maxWidth = lableWidth,
+            scaling = 0.7,
+            text = ""
+    )
+
     override fun init() {
         if (player != null) {
-            addPlayerInventorySlots(player, 8, parent.guiHeight - 58 - 16 - 5 - 12)
+            addPlayerInventorySlots(player, PADDING_LEFT, parent.guiHeight - 58 - 16 - 5 - 12)
         }
 
         for (i in 0 until upgrades.numberOfUpgradeSlots) {
-            components.add(TCSlotIO(upgrades.inventory, i, 8 + (i % 4) * 18, 20 + (i / 4) * 18, parent))
+            components.add(TCSlotIO(upgrades.inventory, i,
+                    PADDING_LEFT + (i % UPGRADE_SLOTS_PER_ROW) * SLOT_WIDTH_PLUS_PADDING,
+                    PADDING_TOP + (i / UPGRADE_SLOTS_PER_ROW) * SLOT_WIDTH_PLUS_PADDING, parent))
         }
+
+        components.add(infoTitleLabel)
+        components.add(infoContentLabel)
     }
 
     override fun draw(x: Int, y: Int, mouseX: Int, mouseY: Int, partialTicks: Float) {
-        super.draw(x, y, mouseX, mouseY, partialTicks)
-
-        var deltaY = 20
-        val fr = Minecraft.getMinecraft().fontRenderer
-        upgrades.description.forEach { (title, data) ->
-            parent.drawString(fr, title.formattedText, 85 + x, deltaY + y, 0xffffff)
-            parent.drawString(fr, data.formattedText,
-                    parent.guiWidth - 15 - fr.getStringWidth(data.unformattedText) + x, deltaY + y, 0xffffff)
-            deltaY += 12
+        val infoText = StringBuffer()
+        upgrades.description.forEach { (title, _) ->
+            infoText.append(title.formattedText + "\n")
         }
+        this.infoTitleLabel.text = infoText.toString()
+
+        infoText.setLength(0)
+        upgrades.description.forEach { (_, data) ->
+            infoText.append(data.formattedText + "\n")
+        }
+        this.infoContentLabel.text = infoText.toString()
+
+
+        super.draw(x, y, mouseX, mouseY, partialTicks)
     }
 }
