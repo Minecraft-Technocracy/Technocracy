51f363d5f928b174c79a7a4d9151e63e5b2d826b
Cydhra <ubezl@student.kit.edu>
2020-03-04 19:55:09 +0100

    added laser logic to laser TE

--- technocracy.optics/src/main/kotlin/net/cydhra/technocracy/optics/content/tileentities/machines/TileEntityLaser.kt
+++ technocracy.optics/src/main/kotlin/net/cydhra/technocracy/optics/content/tileentities/machines/TileEntityLaser.kt
@@ -2,18 +2,28 @@ package net.cydhra.technocracy.optics.content.tileentities.machines
 
 import net.cydhra.technocracy.foundation.client.gui.TCContainer
 import net.cydhra.technocracy.foundation.client.gui.TCGui
+import net.cydhra.technocracy.foundation.content.tileentities.components.EnergyStorageTileEntityComponent
 import net.cydhra.technocracy.foundation.model.tileentities.api.TCMachineTileEntity
 import net.cydhra.technocracy.foundation.model.tileentities.api.logic.ILogicClient
 import net.cydhra.technocracy.foundation.model.tileentities.api.logic.LogicClientDelegate
 import net.cydhra.technocracy.foundation.model.tileentities.impl.AggregatableTileEntity
 import net.cydhra.technocracy.optics.api.tileentities.components.LaserEmitterTileEntityComponent
+import net.cydhra.technocracy.optics.api.tileentities.logic.LaserLogic
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.EnumFacing
 
 class TileEntityLaser : AggregatableTileEntity(), TCMachineTileEntity, ILogicClient by LogicClientDelegate() {
 
+    private val energyStorage = EnergyStorageTileEntityComponent(facing = mutableSetOf(EnumFacing.DOWN))
+    private val laserEmitter = LaserEmitterTileEntityComponent(setOf(EnumFacing.NORTH), limit = -1)
+
     init {
-        this.registerComponent(LaserEmitterTileEntityComponent(setOf(EnumFacing.NORTH), limit = -1), "laser_emitter")
+        this.registerComponent(laserEmitter, "laser_emitter")
+    }
+
+    override fun onLoad() {
+        this.addLogicStrategy(LaserLogic(this.pos, this.world, this.laserEmitter, this.energyStorage),
+                "laser_logic")
     }
 
     override fun update() {
