2e85a3eeeef2a454409996a8d9e8bec5b13e04cb
Cydhra <ubezl@student.kit.edu>
2020-03-05 16:21:20 +0100

    documentation of default laser capability implementations

--- technocracy.optics/src/main/kotlin/net/cydhra/technocracy/optics/api/capabilities/laser/DefaultLaserEmitter.kt
+++ technocracy.optics/src/main/kotlin/net/cydhra/technocracy/optics/api/capabilities/laser/DefaultLaserEmitter.kt
@@ -2,6 +2,10 @@ package net.cydhra.technocracy.optics.api.capabilities.laser
 
 import net.minecraft.util.EnumFacing
 
+/**
+ * The default implementation forge requires to exist. This does only throw exceptions when used, so another
+ * implementation must be provided whenever the capability is used.
+ */
 class DefaultLaserEmitter : ILaserEmitter {
     override fun stopTransmission(facing: EnumFacing) {
         TODO("not implemented")
