4df8a61179b6e4b460d86bd9951d6fc38eefeeb8
Cydhra <ubezl@student.kit.edu>
2020-03-28 16:26:14 +0100

    implemented PipeContent.isEmpty

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/types/PipeContent.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduits/types/PipeContent.kt
@@ -9,10 +9,22 @@ import net.minecraftforge.items.IItemHandler
 /**
  * A wrapper type to abstract pipe content for the transfer algorithms
  */
-abstract class PipeContent()
+abstract class PipeContent() {
+    /**
+     * Whether the pipe content package has a content size of 0. It will have a content type, but no amount of its
+     * content.
+     */
+    abstract fun isEmpty(): Boolean
+}
 
-data class PipeItemContent(val source: IItemHandler, val simulatedStack: ItemStack) : PipeContent()
+data class PipeItemContent(val source: IItemHandler, val simulatedStack: ItemStack) : PipeContent() {
+    override fun isEmpty(): Boolean = simulatedStack.isEmpty
+}
 
-data class PipeFluidContent(val source: IFluidHandler, val simulatedStack: FluidStack) : PipeContent()
+data class PipeFluidContent(val source: IFluidHandler, val simulatedStack: FluidStack) : PipeContent() {
+    override fun isEmpty(): Boolean = simulatedStack.amount == 0
+}
 
-data class PipeEnergyContent(val source: IEnergyStorage, val amount: Int) : PipeContent()
+data class PipeEnergyContent(val source: IEnergyStorage, val amount: Int) : PipeContent() {
+    override fun isEmpty(): Boolean = amount == 0
+}
