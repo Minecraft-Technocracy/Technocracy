25cbd5884910e518d51a8eddb7dd3954353a793c
Poke <platin_one@yahoo.de>
2020-03-31 21:14:47 +0200

    upgrade components dont get added twice in guis

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/model/tileentities/machines/MachineTileEntity.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/model/tileentities/machines/MachineTileEntity.kt
@@ -19,6 +19,8 @@ import net.cydhra.technocracy.foundation.content.capabilities.fluid.DynamicFluid
 import net.cydhra.technocracy.foundation.content.capabilities.inventory.DynamicInventoryCapability
 import net.cydhra.technocracy.foundation.content.tileentities.components.*
 import net.cydhra.technocracy.foundation.content.tileentities.logic.RedstoneLogic
+import net.cydhra.technocracy.foundation.content.tileentities.upgrades.CoolingUpgrade
+import net.cydhra.technocracy.foundation.content.tileentities.upgrades.LubricantUpgrade
 import net.cydhra.technocracy.foundation.content.tileentities.upgrades.MACHINE_UPGRADE_ENERGY
 import net.cydhra.technocracy.foundation.content.tileentities.upgrades.MACHINE_UPGRADE_SPEED
 import net.cydhra.technocracy.foundation.model.tileentities.api.TCMachineTileEntity
@@ -77,67 +79,70 @@ open class MachineTileEntity : AggregatableTileEntity(), TCMachineTileEntity, IL
                 var inputNearestToTheMiddle = 0
                 var outputNearestToTheMiddle = parent.guiWidth
                 var foundProgressComponent: ProgressTileEntityComponent? = null
+
                 val sortedComponents = listOf(*this@MachineTileEntity.getComponents().toTypedArray())
                         .sortedBy { (_, component) -> component !is FluidTileEntityComponent }
                         .sortedBy { (_, component) -> component !is EnergyStorageTileEntityComponent }
                 sortedComponents.forEach { (name, component) ->
-                    when (component) {
-                        is EnergyStorageTileEntityComponent -> {
-                            components.add(DefaultEnergyMeter(nextInput, 20, component, gui))
-                            if (inputNearestToTheMiddle < 20) {
-                                inputNearestToTheMiddle = 20
-                                nextInput = 25
+                    if (name != CoolingUpgrade.COOLER_FLUID_INPUT_NAME && name != CoolingUpgrade.COOLER_FLUID_OUTPUT_NAME && name != LubricantUpgrade.LUBRICANT_FLUID_COMPONENT_NAME) {
+                        when (component) {
+                            is EnergyStorageTileEntityComponent -> {
+                                components.add(DefaultEnergyMeter(nextInput, 20, component, gui))
+                                if (inputNearestToTheMiddle < 20) {
+                                    inputNearestToTheMiddle = 20
+                                    nextInput = 25
+                                }
                             }
-                        }
-                        is FluidTileEntityComponent -> {
-                            when {
-                                component.fluid.tanktype == DynamicFluidCapability.TankType.INPUT -> {
-                                    components.add(DefaultFluidMeter(nextInput, 20, component, gui))
-                                    if (inputNearestToTheMiddle < nextInput - 5) {
-                                        inputNearestToTheMiddle = nextInput - 5 // 5 is the space between components
+                            is FluidTileEntityComponent -> {
+                                when {
+                                    component.fluid.tanktype == DynamicFluidCapability.TankType.INPUT -> {
+                                        components.add(DefaultFluidMeter(nextInput, 20, component, gui))
+                                        if (inputNearestToTheMiddle < nextInput - 5) {
+                                            inputNearestToTheMiddle = nextInput - 5 // 5 is the space between components
+                                        }
+                                        nextInput += 25 // fluid meter width (10) + space (5)
                                     }
-                                    nextInput += 25 // fluid meter width (10) + space (5)
-                                }
-                                component.fluid.tanktype == DynamicFluidCapability.TankType.OUTPUT -> {
-                                    components.add(DefaultFluidMeter(nextOutput, 20, component, gui))
-                                    if (outputNearestToTheMiddle > nextOutput)
-                                        outputNearestToTheMiddle = nextOutput
-                                    nextOutput += 25
-                                }
-                                component.fluid.tanktype == DynamicFluidCapability.TankType.BOTH -> {
-                                    TODO("not implemented")
+                                    component.fluid.tanktype == DynamicFluidCapability.TankType.OUTPUT -> {
+                                        components.add(DefaultFluidMeter(nextOutput, 20, component, gui))
+                                        if (outputNearestToTheMiddle > nextOutput)
+                                            outputNearestToTheMiddle = nextOutput
+                                        nextOutput += 25
+                                    }
+                                    component.fluid.tanktype == DynamicFluidCapability.TankType.BOTH -> {
+                                        TODO("not implemented")
+                                    }
                                 }
                             }
-                        }
-                        is InventoryTileEntityComponent -> {
-                            if (component.inventoryType != DynamicInventoryCapability.InventoryType.OUTPUT) {
-                                for (i in 0 until component.inventory.slots) {
-                                    if (nextInput == 25)
-                                        nextInput = 30
-                                    val slot = TCSlotIO(component.inventory, i, nextInput, 40, gui)
-                                    slot.type = component.inventoryType
-                                    components.add(slot)
-                                    if (inputNearestToTheMiddle < nextInput)
-                                        inputNearestToTheMiddle = nextInput
-                                    nextInput += 20
-                                }
+                            is InventoryTileEntityComponent -> {
+                                if (component.inventoryType != DynamicInventoryCapability.InventoryType.OUTPUT) {
+                                    for (i in 0 until component.inventory.slots) {
+                                        if (nextInput == 25)
+                                            nextInput = 30
+                                        val slot = TCSlotIO(component.inventory, i, nextInput, 40, gui)
+                                        slot.type = component.inventoryType
+                                        components.add(slot)
+                                        if (inputNearestToTheMiddle < nextInput)
+                                            inputNearestToTheMiddle = nextInput
+                                        nextInput += 20
+                                    }
 
-                            } else {
-                                for (i in component.inventory.slots - 1 downTo 0) {
-                                    val slot = TCSlotIO(component.inventory, i, 125 + i * 20, 40, gui)
-                                    slot.type = component.inventoryType
-                                    components.add(slot)
-                                    val newX = 125 + i * 20
-                                    if (outputNearestToTheMiddle > newX)
-                                        outputNearestToTheMiddle = newX
+                                } else {
+                                    for (i in component.inventory.slots - 1 downTo 0) {
+                                        val slot = TCSlotIO(component.inventory, i, 125 + i * 20, 40, gui)
+                                        slot.type = component.inventoryType
+                                        components.add(slot)
+                                        val newX = 125 + i * 20
+                                        if (outputNearestToTheMiddle > newX)
+                                            outputNearestToTheMiddle = newX
+                                    }
                                 }
                             }
-                        }
-                        is ProgressTileEntityComponent -> {
-                            foundProgressComponent = component
+                            is ProgressTileEntityComponent -> {
+                                foundProgressComponent = component
+                            }
                         }
                     }
-                }
+            }
                 if (foundProgressComponent != null)
                     components.add(DefaultProgressBar((outputNearestToTheMiddle - inputNearestToTheMiddle) / 2 + inputNearestToTheMiddle,
                             40,
