788684a50050ad3a4cbf7455f6a1c9556978a8c1
Cydhra <ubezl@student.kit.edu>
2020-04-10 15:27:12 +0200

    introduced TCAggregatableItemStack

---
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/api/ecs/item/TCAggregatableItemStack.kt
@@ -0,0 +1,10 @@
+package net.cydhra.technocracy.foundation.api.ecs.item
+
+import net.cydhra.technocracy.foundation.api.ecs.IAggregatable
+import net.cydhra.technocracy.foundation.model.tileentities.api.components.AbstractTileEntityComponent
+
+/**
+ * Aggregation of [AbstractTileEntityComponent] implementations used for itemstacks. Note, that this interface is not
+ * necessarily implemented by an itemstack, but can be implemented as a delegate instead.
+ */
+interface TCAggregatableItemStack : IAggregatable
