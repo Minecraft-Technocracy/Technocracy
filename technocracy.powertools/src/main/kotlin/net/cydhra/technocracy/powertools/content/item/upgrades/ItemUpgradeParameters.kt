a283e8589ae05f39b6467ed8fe1e5651ec366b69
Cydhra <ubezl@student.kit.edu>
2020-04-18 18:43:11 +0200

    some upgrade parameters

---
+++ technocracy.powertools/src/main/kotlin/net/cydhra/technocracy/powertools/content/item/upgrades/ItemUpgradeParameters.kt
@@ -0,0 +1,18 @@
+package net.cydhra.technocracy.powertools.content.item.upgrades
+
+import net.cydhra.technocracy.foundation.api.upgrades.UpgradeParameter
+
+// general
+const val UPGRADE_ENERGY_CAPACITY: UpgradeParameter = "energy.capacity"
+const val UPGRADE_ENERGY_USAGE: UpgradeParameter = "energy.usage"
+
+// tool
+const val UPGRADE_DIG_SPEED: UpgradeParameter = "dig.speed"
+
+// armor
+const val UPGRADE_ARMOR: UpgradeParameter = "armor"
+const val UPGRADE_WALK_SPEED: UpgradeParameter = "walk.speed"
+
+// weapon
+const val UPGRADE_ATTACK_SPEED: UpgradeParameter = "attack.speed"
+const val UPGRADE_ATTACK_DAMAGE: UpgradeParameter = "attack.damage"
